0fba92953618cf6b4c06e5a8d13c004e4eccd3aa
jinwook567 <jinwook567@we2d.com>
2022-12-31 14:18:25 +0900

    refactor: ImageAssets test 선언적으로 변경

--- src/components/ImageAssets/ImageAssets.test.tsx
+++ src/components/ImageAssets/ImageAssets.test.tsx
@@ -6,7 +6,7 @@ import { RecoilObserver } from '../../utils/test';
 import { stagesState } from '../../recoil/editor';
 import { createNode } from '../../utils/editor';
 
-test('ImageAssets 컴포넌트 테스트', async () => {
+function renderImageAssets() {
   const onChange = jest.fn();
   render(
     <RecoilRoot>
@@ -15,43 +15,77 @@ test('ImageAssets 컴포넌트 테스트', async () => {
     </RecoilRoot>
   );
 
-  const input = (await screen.findByTestId('image-input')).querySelector(
-    'input'
-  );
+  const Input = () => screen.getByTestId('image-input').querySelector('input');
 
-  const submit = await screen.findByTestId('image-submit');
+  const SubmitButton = () => screen.getByTestId('image-submit');
 
-  if (!input) throw new Error('no input');
+  const Images = () => screen.getAllByRole('img');
 
-  const inputValue1 = 'ddddd1';
-  const inputValue2 = 'ddddd2';
+  const changeInput = (inputValue: string) => {
+    const inputElement = Input();
+    if (!inputElement) throw new Error('no input element');
 
-  fireEvent.change(input, { target: { value: inputValue1 } });
-  fireEvent.submit(submit);
+    fireEvent.change(inputElement, { target: { value: inputValue } });
+  };
 
-  fireEvent.change(input, { target: { value: inputValue2 } });
-  fireEvent.submit(submit);
+  const clickSubmit = () => {
+    fireEvent.submit(SubmitButton());
+  };
 
-  const images = await screen.findAllByRole('img');
+  return {
+    Input,
+    SubmitButton,
+    Images,
+    changeInput,
+    clickSubmit,
+    onChange,
+  };
+}
 
-  expect(images[0]).toBeInTheDocument();
-  expect(images[0].getAttribute('src')).toBe(inputValue1);
+test('Image Asset url로 추가 테스트', async () => {
+  const { Input, SubmitButton, changeInput, clickSubmit, Images } =
+    renderImageAssets();
 
-  expect(images[1]).toBeInTheDocument();
-  expect(images[1].getAttribute('src')).toBe(inputValue2);
+  expect(Input()).toBeInTheDocument();
+  expect(SubmitButton()).toBeInTheDocument();
 
-  fireEvent.change(input, { target: { value: inputValue2 } });
-  fireEvent.submit(submit);
+  changeInput('value1');
+  clickSubmit();
 
-  const addedImages = await screen.findAllByRole('img');
-  const firstImage = addedImages[0];
-  expect(firstImage.getAttribute('src')).toBe(inputValue2);
+  expect(Images()[0].getAttribute('src')).toBe('value1');
 
-  // 이미지 Stage 추가 로직 테스트
-  fireEvent.click(firstImage);
+  changeInput('value2');
+  clickSubmit();
 
-  const url = firstImage.getAttribute('src');
+  expect(Images()[1]).toBeInTheDocument();
+  expect(Images()[1].getAttribute('src')).toBe('value2');
+});
 
+test('Image Assest 동일한 url이 주입될 경우 순서 변경 테스트', () => {
+  const { changeInput, clickSubmit, Images } = renderImageAssets();
+
+  changeInput('value1');
+  clickSubmit();
+
+  changeInput('value2');
+  clickSubmit();
+
+  changeInput('value2');
+  clickSubmit();
+
+  expect(Images()[0].getAttribute('src')).toBe('value2');
+  expect(Images()[1].getAttribute('src')).toBe('value1');
+});
+
+test('Image 클릭 시 Stage로 추가가 잘 되는지 테스트', () => {
+  const { Images, changeInput, clickSubmit, onChange } = renderImageAssets();
+
+  changeInput('value1');
+  clickSubmit();
+
+  fireEvent.click(Images()[0]);
+
+  const url = Images()[0].getAttribute('src');
   const node = url
     ? createNode({
         type: 'image',
@@ -65,5 +99,4 @@ test('ImageAssets 컴포넌트 테스트', async () => {
   expect(onChange).toBeCalledWith(
     expect.arrayContaining([[expect.objectContaining(rest)]])
   );
-  // Stage가 여러개 될 때 해당 배열에 잘 들어가는지도 테스트 해야함.
 });
