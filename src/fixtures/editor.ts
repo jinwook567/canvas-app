09f18a7efccfe0bc7f44b9228e96c414d510ac2a
jinwook567 <jinwook567@we2d.com>
2023-01-02 14:28:41 +0900

    feat: test를 위한 상수 추가

---
+++ src/fixtures/editor.ts
@@ -0,0 +1,7 @@
+export const imageAssets = [
+  {
+    url: 'https://images.unsplash.com/photo-1481349518771-20055b2a7b24?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cmFuZG9tfGVufDB8fDB8fA%3D%3D&w=1000&q=80',
+  },
+];
+
+export default {};
