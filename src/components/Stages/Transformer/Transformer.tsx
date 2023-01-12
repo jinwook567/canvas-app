9de551cb0fedc61b1d13cb402481f3fc1b4dd9d5
jinwook567 <jinwook567@we2d.com>
2023-01-12 12:16:13 +0900

    refactor: useTransform hook 적용 및 핸들러 함수 리팩토링

--- src/components/Stages/Transformer/Transformer.tsx
+++ src/components/Stages/Transformer/Transformer.tsx
@@ -1,42 +1,46 @@
-import Konva from 'konva';
 import React, { RefObject } from 'react';
+import { KonvaEventObject, Node, NodeConfig } from 'konva/lib/Node';
+import Konva from 'konva';
 import * as ReactKonva from 'react-konva';
-import useEditor from '../../../hooks/useEditor';
+import useTransform from '../../../hooks/useTransform';
 
 type Props = {
   trRef: RefObject<Konva.Transformer>;
 };
 
 function Transformer({ trRef }: Props) {
-  const { handleTransformNodes } = useEditor();
+  const { onTransformEnd } = useTransform();
+
+  const getDraggedNodeAttrs = (node: Node<NodeConfig>) => ({
+    id: node.id(),
+    x: node.x(),
+    y: node.y(),
+  });
+  const getTransformedNodeAttrs = (node: Node<NodeConfig>) => ({
+    ...getDraggedNodeAttrs(node),
+    scaleX: node.scaleX(),
+    scaleY: node.scaleY(),
+    rotation: node.rotation(),
+  });
+
+  const handleTransformEnd = (e: KonvaEventObject<Event>) => {
+    if (!trRef.current) return;
+
+    const { type } = e;
+    const transformedNodes = trRef.current
+      .nodes()
+      .map(
+        type === 'transformend' ? getTransformedNodeAttrs : getDraggedNodeAttrs
+      );
+
+    onTransformEnd(transformedNodes);
+  };
+
   return (
     <ReactKonva.Transformer
       ref={trRef}
-      onTransformEnd={() => {
-        if (!trRef.current) return;
-
-        const transformedNodes = trRef.current.nodes().map(trNode => ({
-          id: trNode.id(),
-          x: trNode.x(),
-          y: trNode.y(),
-          scaleX: trNode.scaleX(),
-          scaleY: trNode.scaleY(),
-          rotation: trNode.rotation(),
-        }));
-
-        handleTransformNodes(transformedNodes);
-      }}
-      onDragEnd={() => {
-        if (!trRef.current) return;
-
-        const transformedNodes = trRef.current.nodes().map(trNode => ({
-          id: trNode.id(),
-          x: trNode.x(),
-          y: trNode.y(),
-        }));
-
-        handleTransformNodes(transformedNodes);
-      }}
+      onTransformEnd={handleTransformEnd}
+      onDragEnd={handleTransformEnd}
     />
   );
 }
