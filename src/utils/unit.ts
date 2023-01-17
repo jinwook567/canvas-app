2e0cc9d6694bd9422b82fc0943b3aaae67abe748
jinwook567 <jinwook567@we2d.com>
2023-01-17 13:25:00 +0900

    feat: 객체의 특정 속성만을 추출하는 함수 정의

--- src/utils/unit.ts
+++ src/utils/unit.ts
@@ -2,4 +2,26 @@ import { v4 as uuidv4 } from 'uuid';
 
 export const createUniqueId = () => uuidv4();
 
+export const pick = <T, K extends keyof T>(
+  obj: T,
+  ...rest: K[]
+): Pick<T, K> => {
+  const newObj = {} as Pick<T, K>;
+  rest.forEach(key => {
+    newObj[key] = obj[key];
+  });
+  return newObj;
+};
+
+export const omit = <T, K extends keyof T>(
+  obj: T,
+  ...rest: K[]
+): Omit<T, K> => {
+  const newObj = { ...obj };
+  rest.forEach(key => {
+    delete newObj[key];
+  });
+  return newObj;
+};
+
 export default {};
