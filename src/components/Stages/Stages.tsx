ebd61721ca5f33b7290067e71262ebc416ac7e06
jinwook567 <jinwook567@we2d.com>
2023-01-17 12:32:10 +0900

    feat: history handler 임시 할당

--- src/components/Stages/Stages.tsx
+++ src/components/Stages/Stages.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import { useRecoilValue } from 'recoil';
+import useEditorHistory from '../../hooks/useEditorHistory';
 import { stagesState } from '../../recoil/editor';
 import Layer from './Layer/Layer';
 import Stage from './Stage/Stage';
 
 function Stages() {
   const stages = useRecoilValue(stagesState);
+  const { historyBack, historyForward } = useEditorHistory();
 
   return (
     <div>
+      <button type="button" onClick={historyBack}>
+        back
+      </button>
+      <button type="button" onClick={historyForward}>
+        forward
+      </button>
       {stages.map((nodes, index) => (
         <Stage key={`${stages.length + index}`} index={index}>
           <Layer nodes={nodes} />
