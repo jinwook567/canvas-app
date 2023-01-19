91ab8c956bda6e1f2b7f2102e13aa8f26a604e53
jinwook567 <jinwook567@we2d.com>
2023-01-19 16:02:05 +0900

    feat: Config default options

--- src/constants/editor.ts
+++ src/constants/editor.ts
@@ -1,3 +1,28 @@
+import {
+  DefaultKonvaGroupConfig,
+  DefaultKonvaImageConfig,
+  DefaultKonvaTextConfig,
+} from '../types/editor';
+
 export const initialImageStageRatio = 0.3;
+
+export const defaultTextConfig: DefaultKonvaTextConfig = {
+  scaleX: 1,
+  scaleY: 1,
+  align: 'center' as const,
+  verticalAlign: 'top' as const,
+};
+
+export const defaultImageConfig: DefaultKonvaImageConfig = {
+  scaleX: 1,
+  scaleY: 1,
+};
+
+export const defaultGroupConfig: DefaultKonvaGroupConfig = {
+  scaleX: 1,
+  scaleY: 1,
+  x: 0,
+  y: 0,
+};
 
 export default {};
