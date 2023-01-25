f0dd5400751af20a027cfb03c3b74056ddaf6417
jinwook567 <jinwook567@we2d.com>
2023-01-25 13:04:10 +0900

    feat: stage 변수 생성

--- src/fixtures/editor.ts
+++ src/fixtures/editor.ts
@@ -4,4 +4,66 @@ export const imageAssets = [
   },
 ];
 
-export default {};
+const nodes = [
+  {
+    type: 'image' as const,
+    url: 'https://images.unsplash.com/photo-1481349518771-20055b2a7b24?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cmFuZG9tfGVufDB8fDB8fA%3D%3D&w=1000&q=80',
+    width: 201.6,
+    height: 124.85800604229607,
+    scaleX: 1,
+    scaleY: 1,
+    id: '3e64b15b-e69d-4364-ad1b-46e19cfe8617',
+    x: 235.2,
+    y: 273.57099697885195,
+  },
+  {
+    type: 'image' as const,
+    url: 'https://images.unsplash.com/photo-1481349518771-20055b2a7b24?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cmFuZG9tfGVufDB8fDB8fA%3D%3D&w=1000&q=80',
+    width: 201.6,
+    height: 124.85800604229607,
+    scaleX: 1,
+    scaleY: 1,
+    id: '26868cfe-a4ed-4cd9-9d4c-353c0c6ab239',
+    x: 250.2,
+    y: 288.57099697885195,
+  },
+  {
+    type: 'image' as const,
+    url: 'https://images.unsplash.com/photo-1481349518771-20055b2a7b24?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cmFuZG9tfGVufDB8fDB8fA%3D%3D&w=1000&q=80',
+    width: 201.6,
+    height: 124.85800604229607,
+    scaleX: 1,
+    scaleY: 1,
+    id: '8815a95f-35c0-49c3-9764-18c49da31321',
+    x: 265.2,
+    y: 303.57099697885195,
+  },
+  {
+    type: 'image' as const,
+    url: 'https://images.unsplash.com/photo-1481349518771-20055b2a7b24?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cmFuZG9tfGVufDB8fDB8fA%3D%3D&w=1000&q=80',
+    width: 201.6,
+    height: 124.85800604229607,
+    scaleX: 1,
+    scaleY: 1,
+    id: '543e6817-289d-46ca-9009-c9284ace425b',
+    x: 280.2,
+    y: 318.57099697885195,
+  },
+  {
+    type: 'image' as const,
+    url: 'https://images.unsplash.com/photo-1481349518771-20055b2a7b24?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cmFuZG9tfGVufDB8fDB8fA%3D%3D&w=1000&q=80',
+    width: 201.6,
+    height: 124.85800604229607,
+    scaleX: 1,
+    scaleY: 1,
+    id: '2ae717f6-2434-4070-b089-526e393adb49',
+    x: 295.2,
+    y: 333.57099697885195,
+  },
+];
+
+export const stage = {
+  width: 515.5,
+  height: 515.5,
+  nodes,
+};
