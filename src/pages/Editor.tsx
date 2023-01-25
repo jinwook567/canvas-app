8ca1144a4c6d40a4c97947c2b4ec730816cc687a
jinwook567 <jinwook567@we2d.com>
2023-01-25 12:34:01 +0900

    feat: Editor page layout change

--- src/pages/Editor.tsx
+++ src/pages/Editor.tsx
@@ -1,19 +1,29 @@
 import React from 'react';
 import { Grid } from '@mui/material';
-import ImageAssets from '../components/Assets/Image/ImageAssetList/ImageAssetList';
 import StageList from '../components/Canvas/StageList/StageList';
+import Container from '../components/Layout/Container/Container';
+import Header from '../components/Layout/Header/Header';
+import Footer from '../components/Layout/Footer/Footer';
+import Main from '../components/Layout/Main/Main';
+import Assets from '../components/Assets/Assets/Assets';
 
 function Editor() {
   return (
-    <Grid container>
-      <Grid item lg={2}>
-        <ImageAssets />
-      </Grid>
+    <Container>
+      <Header>Here is header</Header>
 
-      <Grid item lg={10}>
-        <StageList />
-      </Grid>
-    </Grid>
+      <Main>
+        <Grid lg={3} item container>
+          <Assets />
+        </Grid>
+
+        <Grid item lg={9}>
+          <StageList />
+        </Grid>
+      </Main>
+
+      <Footer>Here is footer</Footer>
+    </Container>
   );
 }
 
