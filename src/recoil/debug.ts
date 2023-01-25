75d9c37c1670bec8463f83d22e4f067612b9a77f
jinwook567 <jinwook567@we2d.com>
2023-01-25 16:18:10 +0900

    feat: Recoil debug

---
+++ src/recoil/debug.ts
@@ -0,0 +1,17 @@
+/* eslint-disable no-restricted-syntax */
+import { useEffect } from 'react';
+import { useRecoilSnapshot } from 'recoil';
+
+function DebugObserver() {
+  const snapshot = useRecoilSnapshot();
+  useEffect(() => {
+    console.debug('The following atoms were modified:');
+    for (const node of snapshot.getNodes_UNSTABLE({ isModified: true })) {
+      console.debug(node.key, snapshot.getLoadable(node));
+    }
+  }, [snapshot]);
+
+  return null;
+}
+
+export default DebugObserver;
