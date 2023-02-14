f8f63245dbdaed4c4974d7a6ce6c0eaf8387a3d4
jinwook567 <jinwook567@we2d.com>
2023-02-15 01:31:25 +0900

    test: useEditorHistory test refactoring

--- src/hooks/__tests__/useEditorHistory.test.tsx
+++ src/hooks/__tests__/useEditorHistory.test.tsx
@@ -2,100 +2,60 @@ import { act } from '@testing-library/react';
 import { imageNodeArg } from '../../fixtures/editor';
 import setupRenderUseEditorHook from '../../utils/setupRenderEditorHook';
 
-test('history', () => {
+function setupRenderHistoryHook() {
   const result = setupRenderUseEditorHook();
-
   const History = () => result.current.history;
-  expect(History()).toEqual([[[]]]);
-
   const AppendAsset = () => result.current.appendAsset;
   const CreateNodeConfig = () => result.current.createNodeConfig;
+  const CurrentStage = () => result.current.currentStage;
+  const OnTransformEnd = () => result.current.onTransformEnd;
+  const HistoryBack = () => result.current.historyBack();
+  const HistoryForward = () => result.current.historyForward();
+  return {
+    History,
+    AppendAsset,
+    CreateNodeConfig,
+    CurrentStage,
+    OnTransformEnd,
+    HistoryBack,
+    HistoryForward,
+  };
+}
+
+test('히스토리 생성', () => {
+  const {
+    History,
+    AppendAsset,
+    CreateNodeConfig,
+    OnTransformEnd,
+    CurrentStage,
+  } = setupRenderHistoryHook();
+
+  act(() => AppendAsset()(CreateNodeConfig()(imageNodeArg)));
+  expect(History().head?.stageList).toEqual([CurrentStage()]);
+
+  act(() => OnTransformEnd()([{ id: CurrentStage()[0].id, x: 50, y: 50 }]));
+  expect(History().head?.stageList).toEqual([CurrentStage()]);
+});
+
+test('히스토리 앞뒤로 이동', () => {
+  const {
+    AppendAsset,
+    CreateNodeConfig,
+    CurrentStage,
+    HistoryBack,
+    HistoryForward,
+  } = setupRenderHistoryHook();
+
+  const prevStage = CurrentStage();
 
   act(() => AppendAsset()(CreateNodeConfig()(imageNodeArg)));
 
-  const CurrentStage = () => result.current.currentStage;
-  const firstStage = CurrentStage();
-  expect(History()).toEqual([[[]], [firstStage]]);
-
-  const historyStages = [firstStage];
-
-  const OnTransformEnd = () => result.current.onTransformEnd;
-
-  for (let i = 0; i < 3; i += 1) {
-    const targetNode = CurrentStage()[0];
-    act(() =>
-      OnTransformEnd()([
-        {
-          id: targetNode.id,
-          x: targetNode.x + 50,
-          y: targetNode.y + 50,
-        },
-      ])
-    );
-    historyStages.push(CurrentStage());
-  }
-
-  const secondStage = historyStages[1];
-  const thirdStage = historyStages[2];
-  const fourthStage = historyStages[3];
-
-  expect(History()).toEqual([
-    [[]],
-    [firstStage],
-    [secondStage],
-    [thirdStage],
-    [fourthStage],
-  ]);
-
-  const HistoryBack = () => result.current.historyBack();
-  const HistoryForward = () => result.current.historyForward();
-
-  expect(CurrentStage()).toEqual(fourthStage);
+  const nextStage = CurrentStage();
 
   act(() => HistoryBack());
-  expect(CurrentStage()).toEqual(thirdStage);
-
-  act(() => HistoryBack());
-  act(() => HistoryBack());
-  expect(CurrentStage()).toEqual(firstStage);
-
-  act(() => HistoryBack());
-  expect(CurrentStage()).toEqual([]);
-
-  act(() => HistoryBack());
-  expect(CurrentStage()).toEqual([]);
+  expect(CurrentStage()).toEqual(prevStage);
 
   act(() => HistoryForward());
-  expect(CurrentStage()).toEqual(firstStage);
-
-  act(() => HistoryForward());
-  act(() => HistoryForward());
-  act(() => HistoryForward());
-  act(() => HistoryForward());
-  act(() => HistoryForward());
-  expect(CurrentStage()).toEqual(fourthStage);
-
-  act(() => HistoryBack());
-  act(() => HistoryBack());
-
-  const targetNode = CurrentStage()[0];
-  act(() =>
-    OnTransformEnd()([
-      {
-        id: targetNode.id,
-        x: targetNode.x + 300,
-        y: targetNode.y + 300,
-      },
-    ])
-  );
-  const newThirdStage = [
-    { ...secondStage[0], x: secondStage[0].x + 300, y: secondStage[0].y + 300 },
-  ];
-
-  expect(History()).toEqual([
-    [[]],
-    [firstStage],
-    [secondStage],
-    [newThirdStage],
-  ]);
+  expect(CurrentStage()).toEqual(nextStage);
 });
