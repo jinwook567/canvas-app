3137e7a7ee3f85f1069a8ff74cfdb145d8cfe416
jinwook567 <jinwook567@we2d.com>
2023-02-15 01:53:32 +0900

    test: useTransform hook test refactoring

--- src/hooks/__tests__/useTransform.test.tsx
+++ src/hooks/__tests__/useTransform.test.tsx
@@ -1,50 +1,31 @@
 import { act } from '@testing-library/react';
-import { createNodeConfig } from '../../utils/editor';
+import { imageNodeArg } from '../../fixtures/editor';
 import setupRenderUseEditorHook from '../../utils/setupRenderEditorHook';
 
-test('handleTransformNodes', () => {
+function setupRenderUseTransformHook() {
   const result = setupRenderUseEditorHook();
-
-  const nodeArg = {
-    url: '1',
-    width: 300,
-    height: 300,
-    type: 'image' as const,
+  return {
+    AppendAsset: () => result.current.appendAsset,
+    OnTransformEnd: () => result.current.onTransformEnd,
+    CurrentStage: () => result.current.currentStage,
+    CreateNodeConfig: () => result.current.createNodeConfig,
   };
+}
 
-  act(() =>
-    result.current.appendAsset(
-      createNodeConfig({
-        nodeArg,
-        stageSize: result.current.stageSize,
-      })
-    )
-  );
-  act(() =>
-    result.current.appendAsset(
-      createNodeConfig({
-        nodeArg,
-        stageSize: result.current.stageSize,
-      })
-    )
-  );
+test('Transform', () => {
+  const { AppendAsset, OnTransformEnd, CurrentStage, CreateNodeConfig } =
+    setupRenderUseTransformHook();
 
-  const transformedNodes = result.current.currentStage.map(node => ({
-    id: node.id,
-    scaleX: node.scaleX * 1.5,
-    scaleY: node.scaleY * 1.5,
+  act(() => AppendAsset()(CreateNodeConfig()(imageNodeArg)));
+  act(() => AppendAsset()(CreateNodeConfig()(imageNodeArg)));
+
+  const transformed = CurrentStage().map(node => ({
+    ...node,
     x: node.x + 100,
-    y: node.y + 200,
   }));
 
-  act(() => result.current.onTransformEnd(transformedNodes));
-  expect(result.current.currentStage).toEqual(
-    transformedNodes.map(node => ({
-      ...node,
-      type: 'image',
-      url: '1',
-      width: 300,
-      height: 300,
-    }))
-  );
+  act(() => OnTransformEnd()(transformed));
+  expect(CurrentStage()).toEqual(transformed);
 });
+
+// TODO: 만일 변형이 요청된 노드가 현재 stage에 없다면 오류를 발생시켜야함.
