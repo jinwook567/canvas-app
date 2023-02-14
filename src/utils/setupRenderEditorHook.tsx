30329c6054fc89eff43f2a5fb4aead7ecd465b31
jinwook567 <jinwook567@we2d.com>
2023-02-15 02:17:11 +0900

    refactor: setupRenderEditorHook
    캡슐화 진행중

--- src/utils/setupRenderEditorHook.tsx
+++ src/utils/setupRenderEditorHook.tsx
@@ -19,4 +19,34 @@ function setupRenderUseEditorHook() {
   return result;
 }
 
+function setupRenderHook() {
+  const wrapper = ({ children }: { children: React.ReactNode }) => (
+    <RecoilRoot>{children}</RecoilRoot>
+  );
+
+  const { result } = renderHook(() => useEditor(), {
+    wrapper,
+  });
+
+  act(() => {
+    result.current.setStageSize({ width: 1000, height: 1000 });
+  });
+
+  return {
+    AppendAsset: () => result.current.appendAsset,
+    SelectShape: () => result.current.selectShape,
+    OrganizeGroup: () => result.current.organizeGroup,
+    CloseGroup: () => result.current.closeGroup,
+    CreateNodeConfig: () => result.current.createNodeConfig,
+    OnTransformEnd: () => result.current.onTransformEnd,
+    CurrentStage: () => result.current.currentStage,
+    StageCount: () => result.current.stageCount,
+    CurrentStageIndex: () => result.current.currentStageIndex,
+    DeleteStage: () => result.current.deleteStage,
+    SelectStage: () => result.current.selectStage,
+    Deselect: () => result.current.deselect,
+    SelectedIds: () => result.current.selectedIds,
+  };
+}
+
 export default setupRenderUseEditorHook;
