c9023ab78aa7fc23ba785015018940e75a1976af
jinwook567 <jinwook567@we2d.com>
2023-02-22 19:12:25 +0900

    feat: forwardRef

--- src/components/canvas2/Image/Image.tsx
+++ src/components/canvas2/Image/Image.tsx
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { ForwardedRef } from 'react';
 import useImage from 'use-image';
 import Konva from 'konva';
 import { Image as KonvaImage } from 'react-konva';
 
 type Props = Konva.ImageConfig | (Konva.ImageConfig & { url: string });
 
-function Image({ url, image, ...rest }: Props) {
+function Image(
+  { url, image, ...config }: Props,
+  ref: ForwardedRef<Konva.Image>
+) {
   return (
-    <KonvaImage {...rest} image={url ? useImage(url, 'anonymous')[0] : image} />
+    <KonvaImage
+      {...config}
+      image={url ? useImage(url, 'anonymous')[0] : image}
+      ref={ref}
+    />
   );
 }
 
-export default Image;
+export default React.forwardRef(Image);
