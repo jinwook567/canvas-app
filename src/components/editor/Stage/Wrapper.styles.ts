e58e9d464f89475fbb03654b7e30e06031f078b4
jinwook567 <jinwook567@we2d.com>
2023-02-23 21:46:26 +0900

    refactor: StageWrapper

---
+++ src/components/editor/Stage/Wrapper.styles.ts
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled';
+
+export const Div = styled.div<{ isSelected: boolean }>`
+  border: 3px solid ${props => (props.isSelected ? 'purple' : 'transparent')};
+
+  &:hover {
+    border: 3px solid purple;
+  }
+`;
---
+++ src/components/editor/Stage/Wrapper.tsx
@@ -0,0 +1,30 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+import React, { useRef, useEffect } from 'react';
+import * as Styled from './Wrapper.styles';
+
+type Props = {
+  isSelected: boolean;
+  onSelect: () => void;
+  children: React.ReactNode;
+};
+
+function Wrapper({ isSelected, onSelect, children }: Props) {
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (ref.current) ref.current.focus();
+  }, [isSelected]);
+
+  const handleSelect = () => {
+    onSelect();
+  };
+
+  return (
+    <Styled.Div onClick={handleSelect} isSelected={isSelected}>
+      {children}
+    </Styled.Div>
+  );
+}
+
+export default Wrapper;
