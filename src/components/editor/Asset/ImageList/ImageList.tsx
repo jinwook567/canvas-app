8c794d03724a75fbf1dd75c6c3671ee2d25cf6a1
jinwook567 <jinwook567@we2d.com>
2023-02-26 19:12:08 +0900

    refactor: ImageAssetList
    ImageAsset이라는 불필요한 컴포넌트 제거하였음. img 태그에 이벤트 핸들러를 직접 할당하여 해당 정보에 접근할 수 있도록 하였음.

---
+++ src/components/editor/Asset/ImageList/ImageList.tsx
@@ -0,0 +1,28 @@
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+import Masonry from '@mui/lab/Masonry';
+import { IconButton } from '@mui/material';
+import React from 'react';
+
+type Props = {
+  items: { src: string }[];
+  onClick: (image: HTMLImageElement) => void;
+};
+
+function ImageList({ items, onClick }: Props) {
+  const handleClick = (e: React.MouseEvent<HTMLImageElement, MouseEvent>) => {
+    onClick(e.currentTarget);
+  };
+
+  return (
+    <Masonry columns={2}>
+      {items.map(({ src }, index) => (
+        <IconButton key={index}>
+          <img src={src} alt="asset" onClick={handleClick} />
+        </IconButton>
+      ))}
+    </Masonry>
+  );
+}
+
+export default ImageList;
