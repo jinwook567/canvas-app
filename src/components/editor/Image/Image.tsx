788816096c920e0839babe01ce5692376740363e
jinwook567 <jinwook567@we2d.com>
2023-02-27 17:47:25 +0900

    refactor: props name 변경

--- src/components/editor/Image/Image.tsx
+++ src/components/editor/Image/Image.tsx
@@ -3,16 +3,16 @@ import useImage from 'use-image';
 import Konva from 'konva';
 import { Image as KonvaImage } from 'react-konva';
 
-type Props = Konva.ImageConfig | (Konva.ImageConfig & { url: string });
+type Props = Konva.ImageConfig | (Konva.ImageConfig & { src: string });
 
 function Image(
-  { url, image, ...config }: Props,
+  { src, image, ...config }: Props,
   ref: ForwardedRef<Konva.Image>
 ) {
   return (
     <KonvaImage
       {...config}
-      image={url ? useImage(url, 'anonymous')[0] : image}
+      image={src ? useImage(src, 'anonymous')[0] : image}
       ref={ref}
     />
   );
