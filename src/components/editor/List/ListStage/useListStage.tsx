c57acec45a22d1a0c52d4ebaf3179246190a71c1
jinwook567 <jinwook567@we2d.com>
2023-03-02 19:22:57 +0900

    feat: useListStage
    ListStage 컴포넌트에 사용되는 props를 제작해주는 커스텀 훅

---
+++ src/components/editor/List/ListStage/useListStage.tsx
@@ -0,0 +1,45 @@
+import useCreate from '../../../../hooks/editor/Stage/useCreate';
+import useDelete from '../../../../hooks/editor/Stage/useDelete';
+import useSelect from '../../../../hooks/editor/Stage/useSelect';
+import { Size } from '../../../../types/editor';
+
+function useListStage() {
+  const { isSelected, changeSelect, getNextId, getPrevId } = useSelect();
+  const getWrapperProps = (id: string) => ({
+    isSelected: isSelected(id),
+    onSelect: () => changeSelect(id),
+  });
+
+  const { deleteStage } = useDelete();
+  const getStageControlBarPropsWithoutOnAppend = (id: string) => {
+    const nextId = getNextId(id);
+    const prevId = getPrevId(id);
+    return {
+      ...(nextId && { onSelectDown: () => changeSelect(nextId) }),
+      ...(prevId && { onSelectUp: () => changeSelect(prevId) }),
+      onDeleteStage: () => deleteStage(id),
+    };
+  };
+
+  const { createStage } = useCreate();
+  const getOnAppendProp = ({
+    size,
+    divSize,
+    id,
+  }: {
+    size: Size;
+    divSize: Size;
+    id: string;
+  }) => ({
+    onAppendStage: () =>
+      createStage({ config: { ...size }, nodes: [] }, divSize, id),
+  });
+
+  return {
+    getWrapperProps,
+    getStageControlBarPropsWithoutOnAppend,
+    getOnAppendProp,
+  };
+}
+
+export default useListStage;
