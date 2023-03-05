ed7e7af2e95992dee38f524fbcaa51c6c842cf15
jinwook567 <jinwook567@we2d.com>
2023-03-05 17:36:02 +0900

    feat: useDelete, 삭제 이후에 자동으로 선택 되도록 변경

--- src/hooks/editor/Stage/useDelete.tsx
+++ src/hooks/editor/Stage/useDelete.tsx
@@ -1,19 +1,32 @@
-import { useSetRecoilState } from 'recoil';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
 import { stagesState } from '../../../recoil/editor/atoms';
+import { stagesCountState } from '../../../recoil/editor/selectors';
 import useSelect from './useSelect';
 
 function useDelete() {
-  const { validateId } = useSelect();
+  const { validateId, changeSelect, getPrevId, getNextId } = useSelect();
   const setStages = useSetRecoilState(stagesState);
+  const stagesCount = useRecoilValue(stagesCountState);
 
   const deleteStage = (id: string) => {
-    if (validateId(id)) {
-      setStages(currentVal =>
-        currentVal.length > 1
-          ? currentVal.filter(stage => stage.id !== id)
-          : currentVal
-      );
+    if (!validateId(id) || stagesCount <= 1) return;
+
+    setStages(currentVal => currentVal.filter(stage => stage.id !== id));
+
+    const prevId = getPrevId(id);
+    const nextId = getNextId(id);
+
+    if (prevId) {
+      changeSelect(prevId);
+      return;
     }
+
+    if (nextId) {
+      changeSelect(nextId);
+      return;
+    }
+
+    if (!prevId && !nextId) throw new Error('there is no possible case');
   };
 
   return { deleteStage };
