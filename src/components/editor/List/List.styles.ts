5f18e86176c811c7803d310d2a95beb819bed90e
jinwook567 <jinwook567@we2d.com>
2023-03-05 16:44:10 +0900

    style: List

---
+++ src/components/editor/List/List.styles.ts
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled';
+
+export const Div = styled.div`
+  width: 100%;
+  background: lightgray;
+  padding-top: 20px;
+  padding-bottom: 20px;
+  display: flex;
+  flex-direction: column;
+  min-height: 100%;
+  max-height: 90vh;
+  overflow: auto;
+  align-items: center;
+`;
