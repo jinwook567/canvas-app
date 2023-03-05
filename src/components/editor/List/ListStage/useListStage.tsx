4a33ed69381edfbbda94f21c8537d1a93455462f
jinwook567 <jinwook567@we2d.com>
2023-03-05 19:11:16 +0900

    feat: props getter Stage component

--- src/components/editor/List/ListStage/useListStage.tsx
+++ src/components/editor/List/ListStage/useListStage.tsx
@@ -1,10 +1,12 @@
 import useCreate from '../../../../hooks/editor/Stage/useCreate';
 import useDelete from '../../../../hooks/editor/Stage/useDelete';
+import useDownload from '../../../../hooks/editor/Stage/useDownload';
 import useSelect from '../../../../hooks/editor/Stage/useSelect';
 import { Size } from '../../../../types/editor';
 
 function useListStage() {
   const { isSelected, changeSelect, getNextId, getPrevId } = useSelect();
+
   const getWrapperProps = (id: string) => ({
     isSelected: isSelected(id),
   });
@@ -39,11 +41,18 @@ function useListStage() {
     onClick: () => changeSelect(id),
   });
 
+  const { isExportRequested, exportURIAsPng } = useDownload();
+  const getStageExportProps = () => ({
+    isExportRequested,
+    onExport: (dataURI: string) => exportURIAsPng(dataURI),
+  });
+
   return {
     getWrapperProps,
     getStageControlBarPropsWithoutOnAppend,
     getOnAppendProp,
     getStageContainerProps,
+    getStageExportProps,
   };
 }
 
