2d615cff8ce26dc7bde3a981ad203404133e24ca
jinwook567 <jinwook567@we2d.com>
2023-03-06 19:24:05 +0900

    feat: KonvaNodeEvents 할당

--- src/components/editor/Node/Node.tsx
+++ src/components/editor/Node/Node.tsx
@@ -1,28 +1,31 @@
 import React, { RefObject, useEffect, useRef } from 'react';
+import { KonvaNodeEvents } from 'react-konva';
 import Konva from 'konva';
-import ShapePicker2 from '../ShapePicker/ShapePicker2';
+import ShapePicker2, {
+  Props as ShapePickerProps,
+} from '../ShapePicker/ShapePicker2';
 
 type Props =
-  | {
+  | ({
       type: 'image';
       config: Konva.ImageConfig;
       src?: string;
       isSelected: boolean;
       updateTransformer: (ref: RefObject<Konva.Image>) => void;
-    }
-  | {
+    } & KonvaNodeEvents)
+  | ({
       type: 'text';
       config: Konva.TextConfig;
       isSelected: boolean;
       updateTransformer: (ref: RefObject<Konva.Text>) => void;
-    }
-  | {
+    } & KonvaNodeEvents)
+  | ({
       type: 'group';
       config: Konva.GroupConfig;
-      nodes: Props[];
+      nodes: ShapePickerProps[];
       isSelected: boolean;
       updateTransformer: (ref: RefObject<Konva.Group>) => void;
-    };
+    } & KonvaNodeEvents);
 
 function Node(props: Props) {
   const { isSelected, updateTransformer, ...rest } = props;
--- src/components/editor/ShapePicker/ShapePicker2.tsx
+++ src/components/editor/ShapePicker/ShapePicker2.tsx
@@ -1,43 +1,43 @@
 import React, { RefObject } from 'react';
-import { Group } from 'react-konva';
+import { Group, KonvaNodeEvents } from 'react-konva';
 import Konva from 'konva';
 import Image from '../Image/Image';
 import Text from '../Text/Text';
 
-type Props =
-  | {
+export type Props =
+  | ({
       type: 'image';
       config: Konva.ImageConfig;
       src?: string;
       nodeRef?: RefObject<Konva.Image>;
-    }
-  | {
+    } & KonvaNodeEvents)
+  | ({
       type: 'text';
       config: Konva.TextConfig;
       nodeRef?: RefObject<Konva.Text>;
-    }
-  | {
+    } & KonvaNodeEvents)
+  | ({
       type: 'group';
       config: Konva.GroupConfig;
       nodes: Props[];
       nodeRef?: RefObject<Konva.Group>;
-    };
+    } & KonvaNodeEvents);
 
 function ShapePicker(props: Props) {
-  const { type, config, nodeRef } = props;
+  const { type, config, nodeRef, ...rest } = props;
 
   switch (type) {
     case 'image':
       const { src } = props;
-      return <Image {...config} ref={nodeRef} src={src} />;
+      return <Image {...config} ref={nodeRef} src={src} {...rest} />;
 
     case 'text':
-      return <Text {...config} ref={nodeRef} />;
+      return <Text {...config} ref={nodeRef} {...rest} />;
 
     case 'group':
       const { nodes } = props;
       return (
-        <Group {...config} ref={nodeRef}>
+        <Group {...config} ref={nodeRef} {...rest}>
           {nodes.map((node, index) => (
             <ShapePicker key={index} {...node} />
           ))}
