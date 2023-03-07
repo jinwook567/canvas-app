b48a343acc59c071d2cb53d18dbb0e34f21d1b33
jinwook567 <jinwook567@we2d.com>
2023-03-07 22:04:47 +0900

    refactor: useTransform
    transformer에서 config만 변경 가능하기에 transformNodesConfig라는 함수를 새롭게 정의하였음.

--- src/hooks/editor/Node/useTransform.tsx
+++ src/hooks/editor/Node/useTransform.tsx
@@ -1,3 +1,5 @@
+import Konva from 'konva';
+import _ from 'lodash';
 import { useSetRecoilState } from 'recoil';
 import { stagesState } from '../../../recoil/editor/atoms';
 import { Node, Stage } from '../../../types/editor';
@@ -5,13 +7,44 @@ import { Node, Stage } from '../../../types/editor';
 function useTransform() {
   const setStages = useSetRecoilState(stagesState);
 
+  function transformNodesConfig(
+    stageId: string,
+    configs: { id: Node['id']; config: Konva.NodeConfig }[]
+  ) {
+    setStages(currentVal =>
+      currentVal.map(stage =>
+        stage.id === stageId
+          ? configs.reduce(
+              (acc: Stage, config) =>
+                checkNodeExist(acc, config.id)
+                  ? replaceConfig(acc, config)
+                  : acc,
+              stage
+            )
+          : stage
+      )
+    );
+
+    function replaceConfig(
+      stage: Stage,
+      config: { id: Node['id']; config: Konva.NodeConfig }
+    ): Stage {
+      const node = stage.nodes.find(node => node.id === config.id);
+      if (!node) return stage;
+
+      const newNode = _.cloneDeep(node);
+      newNode.config = { ...newNode.config, ...config.config };
+      return replaceNode(stage, newNode);
+    }
+  }
+
   function transformNodes(stageId: string, nodes: Node[]) {
     setStages(currentVal =>
       currentVal.map(stage =>
         stage.id === stageId
           ? nodes.reduce(
               (acc, node) =>
-                validateNode(acc, node) ? replaceNode(acc, node) : acc,
+                checkNodeExist(acc, node.id) ? replaceNode(acc, node) : acc,
               stage
             )
           : stage
@@ -21,12 +54,13 @@ function useTransform() {
 
   return {
     transformNodes,
+    transformNodesConfig,
   };
 }
 
-function validateNode(stage: Stage, node: Node) {
-  if (stage.nodes.find(_node => _node.id === node.id)) return true;
-  throw new Error('invalid node');
+function checkNodeExist(stage: Stage, nodeId: string) {
+  if (stage.nodes.find(_node => _node.id === nodeId)) return true;
+  throw new Error('there is no node given id');
 }
 
 function replaceNode(stage: Stage, node: Node) {
