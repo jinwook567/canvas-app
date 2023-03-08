5b8c0f0d6483f8aa7cb6848f801e708ce7cda427
jinwook567 <jinwook567@we2d.com>
2023-03-08 19:24:56 +0900

    feat: TextAsset
    텍스트 노드 생성 기능

---
+++ src/components/editor/Asset/TextAsset/TextAsset.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import useCreate from '../../../../hooks/editor/Node/useCreate';
+import TextList from '../../TextList/TextList';
+
+function TextAsset() {
+  const { createNode } = useCreate();
+  return (
+    <TextList
+      items={[
+        { fontSize: 30, text: '제목 추가하기' },
+        { fontSize: 23, text: '부제목 추가하기' },
+        { fontSize: 15, text: '본문 추가하기' },
+        { fontSize: 11, text: '작은 텍스트 추가하기' },
+      ]}
+      onClick={textConfig => createNode({ type: 'text', config: textConfig })}
+    />
+  );
+}
+
+export default TextAsset;
