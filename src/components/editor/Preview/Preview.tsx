3a3cc51fc86124bb2ba33cde9f4439dc6a53d871
jinwook567 <jinwook567@we2d.com>
2023-03-08 18:27:18 +0900

    fix: Preview
    getResizeScale을 오직 가로 기준으로 값을 받아옴. div 태그의 내용이 없을 경우 height 높이가 없기 때문임.

--- src/components/editor/Preview/Preview.tsx
+++ src/components/editor/Preview/Preview.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Stage, Layer } from 'react-konva';
 import useElementResize from '../../../hooks/editor/useElementSize';
 import { Node } from '../../../types/editor';
+import { getResizeScale } from '../../../utils/editor/scale';
 import ShapePicker2 from '../ShapePicker/ShapePicker2';
 
 export type Props = {
@@ -13,12 +14,20 @@ export type Props = {
 function Preview({ stageWidth, stageHeight, nodes }: Props) {
   const { size, divRef } = useElementResize();
 
+  const scale = getResizeScale(
+    { width: stageWidth, height: stageHeight },
+    { width: size.width, height: size.width },
+    1
+  );
+
   return (
     <div ref={divRef} style={{ width: '100%' }}>
       <Stage
-        width={stageWidth}
-        height={stageHeight}
-        scale={{ x: size.width / stageWidth, y: size.width / stageWidth }}
+        width={stageWidth * scale}
+        height={stageHeight * scale}
+        style={{ border: '1px solid black' }}
+        scaleX={scale}
+        scaleY={scale}
       >
         <Layer>
           {nodes.map(node => (
