bd55cf222fb61a1004eaa2ba46e7a2faa3b9b79a
jinwook567 <jinwook567@we2d.com>
2023-03-08 23:22:32 +0900

    feat: useSideEffects
    global로 사용되는 side effects를 관리하는 커스텀 훅을 정의하였음. GlobalEffect라는 컴포넌트도 정의하였는데, 불필요한 리렌더링을 방지하고자 함임.

---
+++ src/components/editor/GlobalEffect/GlobalEffect.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import useSideEffects from '../../../hooks/editor/Global/useSideEffects';
+
+function GlobalEffect() {
+  useSideEffects();
+  return null;
+}
+
+export default GlobalEffect;
---
+++ src/hooks/editor/Global/useSideEffects.tsx
@@ -0,0 +1,15 @@
+import { useEffect } from 'react';
+import { useRecoilValue } from 'recoil';
+import { stagesState } from '../../../recoil/editor/atoms';
+import useHistory from './useHistory';
+
+function useSideEffects() {
+  const stages = useRecoilValue(stagesState);
+  const { createHistory } = useHistory();
+
+  useEffect(() => {
+    createHistory(stages);
+  }, [stages]);
+}
+
+export default useSideEffects;
