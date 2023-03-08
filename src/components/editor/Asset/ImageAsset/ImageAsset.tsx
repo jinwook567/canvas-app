2391414431acec9212a65c01877f6e66b622a362
jinwook567 <jinwook567@we2d.com>
2023-03-08 19:24:38 +0900

    feat: ImageAsset
    이미지 업로드 및 이미지 노드 생성 기능

---
+++ src/components/editor/Asset/ImageAsset/ImageAsset.tsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import { Grid } from '@mui/material';
+import useCreate from '../../../../hooks/editor/Node/useCreate';
+import ImageList from '../../ImageList/ImageList';
+import ImageUpload from '../../ImageUpload/ImageUpload';
+
+function ImageAsset() {
+  const [items, setItems] = useState<{ src: string }[]>([]);
+  const { createNode } = useCreate();
+  return (
+    <Grid container flexDirection="column" alignItems="center" rowGap={3}>
+      <ImageUpload
+        onSubmit={files => {
+          if (!files) return;
+          for (let i = 0; i < files.length; i += 1) {
+            readImageFileAsURL(files[i], src =>
+              setItems(items => [...items, { src }])
+            );
+          }
+        }}
+      />
+      <ImageList
+        items={items}
+        onClick={image =>
+          createNode({
+            type: 'image',
+            src: image.src,
+            config: {
+              width: image.width,
+              height: image.height,
+              image: undefined,
+            },
+          })
+        }
+      />
+    </Grid>
+  );
+}
+
+function readImageFileAsURL(file: File, callback: (result: string) => void) {
+  const reader = new FileReader();
+  reader.onloadend = () => {
+    if (reader.result) {
+      if (reader.result instanceof ArrayBuffer) {
+        callback(Buffer.from(reader.result).toString('base64'));
+      } else {
+        callback(reader.result);
+      }
+    }
+  };
+  reader.readAsDataURL(file);
+}
+
+export default ImageAsset;
