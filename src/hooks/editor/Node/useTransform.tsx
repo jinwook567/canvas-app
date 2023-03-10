3271e706a7e4db965f8868f553365738376f2745
jinwook567 <jinwook567@we2d.com>
2023-03-10 20:10:46 +0900

    refactor: 변수 가독성 향상, 함수 인라인

--- src/hooks/editor/Node/useTransform.tsx
+++ src/hooks/editor/Node/useTransform.tsx
@@ -1,8 +1,7 @@
 import Konva from 'konva';
-import _ from 'lodash';
 import { useSetRecoilState } from 'recoil';
 import { stagesState } from '../../../recoil/editor/atoms';
-import { Node, Stage } from '../../../types/editor';
+import { Node } from '../../../types/editor';
 import { findStageByNodeId } from '../../../utils/editor/validate';
 
 function useTransform() {
@@ -13,38 +12,43 @@ function useTransform() {
   ) {
     setStages(currentVal =>
       data.reduce((stages, { id, config }) => {
-        const stage = findStageByNodeId(currentVal, id);
-        if (!stage) throw new Error('invalid node');
+        const stageToUpdate = findStageByNodeId(currentVal, id);
+        if (!stageToUpdate) throw new Error('invalid node');
 
-        const newStage = replaceConfig(stage, { id, config });
-        return stages.map(_stage =>
-          _stage.id === newStage.id ? newStage : _stage
+        const nodeToUpdate = stageToUpdate.nodes.find(node => node.id === id);
+        if (!nodeToUpdate) throw new Error('invalid node');
+
+        const updatedStage = {
+          ...stageToUpdate,
+          nodes: stageToUpdate.nodes.map(node =>
+            node === nodeToUpdate
+              ? { ...node, config: { ...node.config, ...config } }
+              : node
+          ),
+        };
+
+        return stages.map(stage =>
+          stage.id === updatedStage.id ? updatedStage : stage
         );
       }, currentVal)
     );
-
-    function replaceConfig(
-      stage: Stage,
-      data: { id: Node['id']; config: Konva.NodeConfig }
-    ): Stage {
-      const node = stage.nodes.find(node => node.id === data.id);
-      if (!node) return stage;
-
-      const newNode = _.cloneDeep(node);
-      newNode.config = { ...newNode.config, ...data.config };
-      return replaceNode(stage, newNode);
-    }
   }
 
   function transformNodes(nodes: Node[]) {
     setStages(currentVal =>
-      nodes.reduce((stages, node) => {
-        const stage = findStageByNodeId(stages, node.id);
-        if (!stage) throw new Error('invalid node');
+      nodes.reduce((stages, nodeToUpdate) => {
+        const stageToUpdate = findStageByNodeId(stages, nodeToUpdate.id);
+        if (!stageToUpdate) throw new Error('invalid node');
 
-        const newStage = replaceNode(stage, node);
-        return stages.map(_stage =>
-          _stage.id === newStage.id ? newStage : _stage
+        const updatedStage = {
+          ...stageToUpdate,
+          nodes: stageToUpdate.nodes.map(node =>
+            node.id === nodeToUpdate.id ? nodeToUpdate : node
+          ),
+        };
+
+        return stages.map(stage =>
+          stage.id === stageToUpdate.id ? updatedStage : stage
         );
       }, currentVal)
     );
@@ -53,13 +57,6 @@ function useTransform() {
   return {
     transformNodes,
     transformNodesConfig,
-  };
-}
-
-function replaceNode(stage: Stage, node: Node) {
-  return {
-    ...stage,
-    nodes: stage.nodes.map(_node => (_node.id === node.id ? node : _node)),
   };
 }
 
