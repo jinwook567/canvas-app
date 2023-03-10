adca018131f16e1b904e2f61f7ffda96696889bb
jinwook567 <jinwook567@we2d.com>
2023-03-10 19:17:43 +0900

    refactor: findByStageByNodeId 함수형으로 변경

--- src/utils/editor/validate.ts
+++ src/utils/editor/validate.ts
@@ -8,11 +8,5 @@ export const areNodesInSameStage = (stages: Stage[], nodeIds: string[]) =>
         findStageByNodeId(stages, nodeIds[index - 1])
   );
 
-export const findStageByNodeId = (stages: Stage[], nodeId: string) => {
-  for (let i = 0; i < stages.length; i += 1) {
-    for (let j = 0; j < stages[i].nodes.length; j += 1) {
-      if (stages[i].nodes[j].id === nodeId) return stages[i];
-    }
-  }
-  return null;
-};
+export const findStageByNodeId = (stages: Stage[], nodeId: string) =>
+  stages.find(stage => stage.nodes.some(node => node.id === nodeId));
