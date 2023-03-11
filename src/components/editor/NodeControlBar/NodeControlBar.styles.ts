e5d2fde476a6e2647cbb9e53451c28cb08089fa9
jinwook567 <jinwook567@we2d.com>
2023-03-11 20:31:11 +0900

    feat: NodeControlBar 레이아웃 정의

---
+++ src/components/editor/NodeControlBar/NodeControlBar.styles.ts
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled';
+import { Grid as MuiGrid } from '@mui/material';
+
+export const Grid = styled(MuiGrid)`
+  background: white;
+  width: 100%;
+  position: sticky;
+  top: 0;
+  z-index: 999;
+`;
---
+++ src/components/editor/NodeControlBar/NodeControlBar.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import * as Styled from './NodeControlBar.styles';
+
+function NodeControlBar() {
+  return <Styled.Grid padding={1}>dd</Styled.Grid>;
+}
+
+export default NodeControlBar;
