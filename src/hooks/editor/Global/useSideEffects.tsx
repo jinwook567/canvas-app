f8b1249d9e1243dbb60d949b09e89bdca7eb7883
jinwook567 <jinwook567@we2d.com>
2023-03-12 19:09:10 +0900

    feat: selectedIds validate

--- src/hooks/editor/Global/useSideEffects.tsx
+++ src/hooks/editor/Global/useSideEffects.tsx
@@ -1,6 +1,8 @@
 import { useEffect } from 'react';
 import { useRecoilValue } from 'recoil';
-import { stagesState } from '../../../recoil/editor/atoms';
+import { selectedIdsState, stagesState } from '../../../recoil/editor/atoms';
+import { selectedStageState } from '../../../recoil/editor/selectors';
+import useSelect from '../Node/useSelect';
 import useHistory from './useHistory';
 
 function useSideEffects() {
@@ -10,6 +12,30 @@ function useSideEffects() {
   useEffect(() => {
     createHistory(stages);
   }, [stages]);
+
+  const selectedStage = useRecoilValue(selectedStageState);
+  const selectedIds = useRecoilValue(selectedIdsState);
+  const { resetSelect, deselect } = useSelect();
+
+  useEffect(() => {
+    if (!selectedStage) {
+      if (selectedIds.length !== 0) resetSelect();
+      return;
+    }
+
+    const areSelectedNodesInSelectedStage = selectedIds.every(selectedId =>
+      selectedStage.nodes.find(node => node.id === selectedId)
+    );
+
+    if (!areSelectedNodesInSelectedStage) {
+      selectedIds
+        .filter(
+          selectedId =>
+            !selectedStage.nodes.find(node => node.id === selectedId)
+        )
+        .forEach(id => deselect(id));
+    }
+  }, [selectedStage, selectedIds]);
 }
 
 export default useSideEffects;
