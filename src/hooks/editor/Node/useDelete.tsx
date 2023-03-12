c9138e15cd99090a99eb349aff6f8cde348effa9
jinwook567 <jinwook567@we2d.com>
2023-03-12 20:51:25 +0900

    feat: useDelete deleteNodes

---
+++ src/hooks/editor/Node/useDelete.tsx
@@ -0,0 +1,34 @@
+import { useSetRecoilState } from 'recoil';
+import { stagesState } from '../../../recoil/editor/atoms';
+import { Node } from '../../../types/editor';
+import {
+  updateStageNodesWithReplacement,
+  updateStages,
+} from '../../../utils/editor/update';
+import { findStageByNodeId } from '../../../utils/editor/validate';
+
+function useDelete() {
+  const setStages = useSetRecoilState(stagesState);
+
+  const deleteNodes = (nodeIds: Node['id'][]) => {
+    setStages(stages =>
+      nodeIds.reduce((acc, nodeId) => {
+        const stageToUpdate = findStageByNodeId(stages, nodeId);
+        if (!stageToUpdate) throw new Error('invalid node');
+
+        const updatedStage = updateStageNodesWithReplacement(
+          stageToUpdate,
+          stageToUpdate.nodes.filter(node => node.id !== nodeId)
+        );
+
+        return updateStages(acc, updatedStage);
+      }, stages)
+    );
+  };
+
+  return {
+    deleteNodes,
+  };
+}
+
+export default useDelete;
