2d6982552e18a3c206a9f9a07d135bafc721d997
jinwook567 <jinwook567@we2d.com>
2023-03-13 20:41:01 +0900

    feat: usePrevious
    이전 value와 새로운 value를 비교하는 함수를 작성하기 위한 hook

---
+++ src/hooks/usePrevious.tsx
@@ -0,0 +1,12 @@
+import { useRef, useEffect } from 'react';
+
+function usePrevious<T>(value: T) {
+  const ref = useRef<T>();
+  useEffect(() => {
+    ref.current = value;
+  }, [value]);
+
+  return ref.current;
+}
+
+export default usePrevious;
