665bccc5d0449e7c590336c6158e69b3280271f0
jinwook567 <jinwook567@we2d.com>
2023-03-15 15:21:55 +0900

    fix: Text
    선택이 해제되면 글자를 수정하는 모드가 풀려야함. 따라서 isSelected를 props로 주입받음.
    
    textarea에 사용되는 width,height를 만들어서 내려보내줌.

--- src/components/common/editor/Text/Text.tsx
+++ src/components/common/editor/Text/Text.tsx
@@ -1,28 +1,53 @@
-import React, { ForwardedRef, useState } from 'react';
+/* eslint-disable react/destructuring-assignment */
+import React, { ForwardedRef, useState, useEffect } from 'react';
 import Konva from 'konva';
 import { Text as KonvaText } from 'react-konva';
 import DomPortal from './DomPortal';
 
 type Props = Konva.TextConfig & {
   onChange: (text: string) => void;
+  isSelected: boolean;
 };
 
-function Text({ onChange, ...config }: Props, ref: ForwardedRef<Konva.Text>) {
+function Text(
+  { onChange, isSelected, ...config }: Props,
+  ref: ForwardedRef<Konva.Text>
+) {
   const [isChangeMode, setIsChangeMode] = useState(false);
 
   const handleDbClick = () => {
     setIsChangeMode(!isChangeMode);
   };
 
-  return isChangeMode ? (
-    <DomPortal
-      x={config.x}
-      y={config.y}
-      text={config.text}
-      onChange={onChange}
-    />
-  ) : (
-    <KonvaText {...config} onDblClick={handleDbClick} ref={ref} />
+  useEffect(() => {
+    if (!isSelected) {
+      setIsChangeMode(false);
+    }
+  }, [isSelected]);
+
+  const width =
+    ref && typeof ref !== 'function' && ref.current
+      ? ref.current.textWidth * (config.scaleX || 1)
+      : 0;
+  const height =
+    ref && typeof ref !== 'function' && ref.current
+      ? ref.current.textHeight *
+        (config.scaleY || 1) *
+        (config.text || '').split('\n').length
+      : 0;
+
+  return (
+    <>
+      {isChangeMode && (
+        <DomPortal
+          config={config}
+          onChange={onChange}
+          width={width}
+          height={height}
+        />
+      )}
+      <KonvaText {...config} onDblClick={handleDbClick} ref={ref} visible />
+    </>
   );
 }
 
