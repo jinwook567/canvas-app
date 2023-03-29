be73265d1c4e7eadf730a539534a28b2d942be22
jinwook567 <jinwook567@we2d.com>
2023-03-30 00:53:18 +0900

    refactor: 불필요한 함수 전부 제거.
    getNextId, getPrevId 함수는 필요한 부분에 인라인 하기로 함. 또한 올바른 stage에 대한 검증은 클라이언트 코드에서 하기로 하였음. stages를 기반으로 배열을 순회하면서 컴포넌트를 생성하는데 stage가 올바르게 사용되지 않을 경우가 적다고 생각해서임.

--- src/hooks/editor/stage/useSelect.tsx
+++ src/hooks/editor/stage/useSelect.tsx
@@ -1,19 +1,10 @@
-import { useRecoilState, useRecoilValue } from 'recoil';
-import {
-  selectedStageIdState,
-  selectedStageState,
-  stagesState,
-} from '../../../recoil/editor/atoms';
+import { useRecoilState } from 'recoil';
+import { selectedStageState } from '../../../recoil/editor/atoms';
 import { Stage } from '../../../types/editor';
 import { isSameStage } from '../../../utils/editor/validate';
 
 function useSelect() {
-  const [selectedStageId, setSelectedStageId] =
-    useRecoilState(selectedStageIdState);
-
   const [selectedStage, setSelectedStage] = useRecoilState(selectedStageState);
-
-  const stages = useRecoilValue(stagesState);
 
   const isSelected = (stage: Stage) =>
     !!(selectedStage && isSameStage(stage, selectedStage));
@@ -22,44 +13,9 @@ function useSelect() {
     setSelectedStage(stage);
   };
 
-  const validateId = (id: string) => {
-    if (!stages.find(stage => stage.id === id))
-      throw new Error('there is no stage given id');
-    return true;
-  };
-
-  const changeSelect = (id: string) => {
-    if (validateId(id)) {
-      setSelectedStageId(id);
-    }
-  };
-
-  const changeSelectWithoutValidate = (id: string) => {
-    setSelectedStageId(id);
-  };
-
-  const getNextId = (id: string) => {
-    if (validateId(id)) {
-      return stages[stages.findIndex(stage => stage.id === id) + 1]?.id || null;
-    }
-    return null;
-  };
-
-  const getPrevId = (id: string) => {
-    if (validateId(id)) {
-      return stages[stages.findIndex(stage => stage.id === id) - 1]?.id || null;
-    }
-    return null;
-  };
-
   return {
     selectStage,
     isSelected,
-    changeSelect,
-    changeSelectWithoutValidate,
-    getNextId,
-    getPrevId,
-    validateId,
   };
 }
 
