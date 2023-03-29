6f0a4101f6dc0e6291a90cca5d41bfb0a27c54e9
jinwook567 <jinwook567@we2d.com>
2023-03-29 21:33:37 +0900

    refactor: ListStage 컴포넌트 분리에 따른 useListStage hook 변경

--- src/components/editor/list/ListStage/useListStage.tsx
+++ src/components/editor/list/ListStage/useListStage.tsx
@@ -1,67 +1,29 @@
-import { KonvaNodeEvents } from 'react-konva';
-import useCreate from '../../../../hooks/editor/stage/useCreate';
-import useDelete from '../../../../hooks/editor/stage/useDelete';
+import { KonvaEventObject } from 'konva/lib/Node';
 import useDownload from '../../../../hooks/editor/stage/useDownload';
-import useSelect from '../../../../hooks/editor/stage/useSelect';
 import useSelectNode from '../../../../hooks/editor/node/useSelect';
-import { Size } from '../../../../types/editor';
 
 function useListStage() {
-  const { isSelected, changeSelect, getNextId, getPrevId } = useSelect();
-
-  const getWrapperProps = (id: string) => ({
-    isSelected: isSelected(id),
-  });
-
-  const { deleteStage } = useDelete();
-  const getStageControlBarPropsWithoutOnAppend = (id: string) => {
-    const nextId = getNextId(id);
-    const prevId = getPrevId(id);
-
-    return {
-      ...(nextId && { onSelectDown: () => changeSelect(nextId) }),
-      ...(prevId && { onSelectUp: () => changeSelect(prevId) }),
-      onDeleteStage: () => deleteStage(id),
-    };
-  };
-
-  const { createStage } = useCreate();
-  const getOnAppendProp = ({
-    size,
-    divSize,
-    id,
-  }: {
-    size: Size;
-    divSize: Size;
-    id: string;
-  }) => ({
-    onAppendStage: () =>
-      createStage({ config: { ...size }, nodes: [] }, divSize, id),
-  });
-
-  const getStageContainerProps = (id: string) => ({
-    onClick: () => changeSelect(id),
-  });
-
   const { isExportRequested, exportURIAsPng } = useDownload();
-  const getStageExportProps = () => ({
+
+  const getExportProps = () => ({
     isExportRequested,
     onExport: (dataURI: string) => exportURIAsPng(dataURI),
   });
 
   const { resetSelect } = useSelectNode();
-  const getStageSelectProps = (): KonvaNodeEvents => ({
-    onTouchStart: e => e.target === e.target.getStage() && resetSelect(),
-    onMouseDown: e => e.target === e.target.getStage() && resetSelect(),
+
+  const handleDeselect = (
+    e: KonvaEventObject<TouchEvent> | KonvaEventObject<MouseEvent>
+  ) => e.target === e.target.getStage() && resetSelect();
+
+  const getDeselectProps = () => ({
+    onTouchStart: handleDeselect,
+    onMouseDown: handleDeselect,
   });
 
   return {
-    getWrapperProps,
-    getStageControlBarPropsWithoutOnAppend,
-    getOnAppendProp,
-    getStageContainerProps,
-    getStageExportProps,
-    getStageSelectProps,
+    getExportProps,
+    getDeselectProps,
   };
 }
 
