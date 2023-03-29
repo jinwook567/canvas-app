61a181c976ab5e5ea14e2f7ec8226aca2576996d
jinwook567 <jinwook567@we2d.com>
2023-03-29 20:35:11 +0900

    refactor: ListStage 컴포넌트 분리

--- src/components/editor/list/ListStage/ListStage.tsx
+++ src/components/editor/list/ListStage/ListStage.tsx
@@ -1,44 +1,26 @@
 import React from 'react';
 import { Size } from '../../../../types/editor';
 import Stage from '../../../common/editor/Stage/Stage';
-import StageControlBar from '../../../common/editor/StageControlBar/StageControlBar';
 import useListStage from './useListStage';
 
 type Props = {
-  id: string;
   size: Size;
-  divSize: Size;
   children: React.ReactNode;
 };
 
-function ListStage({ id, size, divSize, children }: Props) {
-  const {
-    getWrapperProps,
-    getStageControlBarPropsWithoutOnAppend,
-    getOnAppendProp,
-    getStageContainerProps,
-    getStageExportProps,
-    getStageSelectProps,
-  } = useListStage();
+function ListStage({ size, children }: Props) {
+  const { getDeselectProps, getExportProps } = useListStage();
 
   return (
-    <Stage.Wrapper {...getWrapperProps(id)}>
-      <StageControlBar
-        {...getStageControlBarPropsWithoutOnAppend(id)}
-        {...getOnAppendProp({ size, divSize, id })}
-      />
-      <div {...getStageContainerProps(id)}>
-        <Stage
-          width={size.width}
-          height={size.height}
-          style={{ background: 'white' }}
-          {...getStageExportProps()}
-          {...getStageSelectProps()}
-        >
-          {children}
-        </Stage>
-      </div>
-    </Stage.Wrapper>
+    <Stage
+      width={size.width}
+      height={size.height}
+      style={{ background: 'white' }}
+      {...getDeselectProps()}
+      {...getExportProps()}
+    >
+      {children}
+    </Stage>
   );
 }
 
