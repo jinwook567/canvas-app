b61c8d9d068837c52e7ad4a21a3d378f8ae51726
jinwook567 <jinwook567@we2d.com>
2023-03-30 00:55:48 +0900

    refactor: useDelete
    stage가 삭제된 후 자동으로 선택되는 기능을 삭제하였고, 클라이언트에 맡기도록 할 예정임.

--- src/hooks/editor/stage/useDelete.tsx
+++ src/hooks/editor/stage/useDelete.tsx
@@ -1,32 +1,15 @@
-import { useRecoilValue, useSetRecoilState } from 'recoil';
+import { useSetRecoilState } from 'recoil';
 import { stagesState } from '../../../recoil/editor/atoms';
-import { stagesCountState } from '../../../recoil/editor/selectors';
-import useSelect from './useSelect';
+import { Stage } from '../../../types/editor';
+import { isSameStage } from '../../../utils/editor/validate';
 
 function useDelete() {
-  const { validateId, changeSelect, getPrevId, getNextId } = useSelect();
   const setStages = useSetRecoilState(stagesState);
-  const stagesCount = useRecoilValue(stagesCountState);
 
-  const deleteStage = (id: string) => {
-    if (!validateId(id) || stagesCount <= 1) return;
-
-    setStages(currentVal => currentVal.filter(stage => stage.id !== id));
-
-    const prevId = getPrevId(id);
-    const nextId = getNextId(id);
-
-    if (prevId) {
-      changeSelect(prevId);
-      return;
-    }
-
-    if (nextId) {
-      changeSelect(nextId);
-      return;
-    }
-
-    if (!prevId && !nextId) throw new Error('there is no possible case');
+  const deleteStage = (stageToDelete: Stage) => {
+    setStages(currentVal =>
+      currentVal.filter(stage => isSameStage(stage, stageToDelete))
+    );
   };
 
   return { deleteStage };
