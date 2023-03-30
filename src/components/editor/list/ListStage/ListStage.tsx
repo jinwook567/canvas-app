9626b28702daf63882ecd45486a20ea507be83df
jinwook567 <jinwook567@we2d.com>
2023-03-30 16:18:14 +0900

    refactor: ListStage 역할 변경

--- src/components/editor/list/ListStage/ListStage.tsx
+++ src/components/editor/list/ListStage/ListStage.tsx
@@ -1,26 +1,33 @@
 import React from 'react';
-import { Size } from '../../../../types/editor';
+import useSelect from '../../../../hooks/editor/stage/useSelect';
+import { Size, Stage as StageType } from '../../../../types/editor';
 import Stage from '../../../common/editor/Stage/Stage';
 import useListStage from './useListStage';
 
 type Props = {
   size: Size;
   children: React.ReactNode;
+  stage: StageType;
 };
 
-function ListStage({ size, children }: Props) {
+function ListStage({ size, children, stage }: Props) {
   const { getDeselectProps, getExportProps } = useListStage();
+  const { isSelected, selectStage } = useSelect();
 
   return (
-    <Stage
-      width={size.width}
-      height={size.height}
-      style={{ background: 'white' }}
-      {...getDeselectProps()}
-      {...getExportProps()}
-    >
-      {children}
-    </Stage>
+    <Stage.Wrapper isSelected={isSelected(stage)}>
+      <Stage.Selector onSelect={() => selectStage(stage)}>
+        <Stage
+          width={size.width}
+          height={size.height}
+          style={{ background: 'white' }}
+          {...getDeselectProps()}
+          {...getExportProps()}
+        >
+          {children}
+        </Stage>
+      </Stage.Selector>
+    </Stage.Wrapper>
   );
 }
 
--- src/components/editor/list/ListStage/useListStage.tsx
+++ src/components/editor/list/ListStage/useListStage.tsx
@@ -1,6 +1,7 @@
 import { KonvaEventObject } from 'konva/lib/Node';
 import useDownload from '../../../../hooks/editor/stage/useDownload';
 import useSelectNode from '../../../../hooks/editor/node/useSelect';
+import useSelect from '../../../../hooks/editor/stage/useSelect';
 
 function useListStage() {
   const { isExportRequested, exportURIAsPng } = useDownload();
@@ -21,9 +22,13 @@ function useListStage() {
     onMouseDown: handleDeselect,
   });
 
+  const { isSelected, selectStage } = useSelect();
+
   return {
     getExportProps,
     getDeselectProps,
+    isSelected,
+    selectStage,
   };
 }
 
