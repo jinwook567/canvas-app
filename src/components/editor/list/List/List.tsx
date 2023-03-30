a0797850a960d5ce2f0a66f10e07ab25725b060d
jinwook567 <jinwook567@we2d.com>
2023-03-30 17:09:09 +0900

    refactor: List

--- src/components/editor/list/List/List.tsx
+++ src/components/editor/list/List/List.tsx
@@ -3,30 +3,35 @@ import { useRecoilValue } from 'recoil';
 import useElementResize from '../../../../hooks/useElementSize';
 import { stagesState } from '../../../../recoil/editor/atoms';
 import ListStage from '../ListStage/ListStage';
-import useList, { useCreateInitialStage } from './useList';
+import { useCreateInitialStage } from './useList';
 import * as Styled from './List.styles';
 import ListLayerWithTransformableNodes from '../ListLayerWithTransformableNodes/ListLayerWithTransformableNodes';
 import { createStageSize } from '../../../../utils/editor/size';
+import { giveId } from '../../../../hooks/editor/stage/useCreate';
+import ListStageControlBar from '../ListStageControlBar/ListStageControlBar';
 import NodeControlBar from '../../NodeControlBar/NodeControlBar';
-import Stage from '../../../common/editor/Stage/Stage';
 
 function List() {
   const { size, divRef } = useElementResize();
   const stages = useRecoilValue(stagesState);
 
-  useCreateInitialStage(stages, size);
-  const { getStageWrapperProps } = useList();
+  const initialStage = { config: { width: 500, height: 500 }, nodes: [] };
+  useCreateInitialStage(stages.length, giveId(initialStage), size);
 
   return (
     <Styled.Grid ref={divRef} rowGap={3}>
       <NodeControlBar />
-
-      {stages.map(stage => (
-        <Stage.Wrapper key={stage.id} {...getStageWrapperProps(stage)}>
-          <ListStage size={createStageSize(stage).size}>
+      {stages.map((stage, index) => (
+        <div key={stage.id}>
+          <ListStageControlBar
+            stage={stage}
+            prevStage={stages[index - 1]}
+            nextStage={stages[index + 1]}
+          />
+          <ListStage size={createStageSize(stage).size} stage={stage}>
             <ListLayerWithTransformableNodes nodes={stage.nodes} />
           </ListStage>
-        </Stage.Wrapper>
+        </div>
       ))}
     </Styled.Grid>
   );
