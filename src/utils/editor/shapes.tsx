701294c3da30d65e41e5ea553363eee32ba1f674
jinwook567 <jinwook567@we2d.com>
2023-04-14 15:11:32 +0900

    refactor: 요소 객체 지향적으로 재설계
    클래스 내부에 ref를 위치 시킴으로써 요소에 직접 접근하여 Konva 에 구현된 다양한 내부 메소드들을 사용할 수 있다.
    
    render를 위치시킴으로써 개방 폐쇄 원칙을 준수할 수 있게 되었다. 기존에는 요소가 추가되면 ShapePicker에 추가해주어야 했음.

---
+++ src/utils/editor/shapes.tsx
@@ -0,0 +1,106 @@
+/* eslint-disable max-classes-per-file */
+import React, { RefObject, useRef } from 'react';
+import Konva from 'konva';
+import { ImageConfig } from 'konva/lib/shapes/Image';
+import { TextConfig } from 'konva/lib/shapes/Text';
+import { GroupConfig } from 'konva/lib/Group';
+import {
+  Image as ImageComponent,
+  Text as TextComponent,
+  Group as GroupComponent,
+} from 'react-konva';
+import { ContainerConfig } from 'konva/lib/Container';
+import { createUniqueId } from '../unit';
+
+export interface RefConfig<RefType, ConfigType> {
+  current: RefObject<RefType>['current'];
+  config: ConfigType;
+  id: string;
+}
+
+export interface Utils<Ref, Config> extends RefConfig<Ref, Config> {
+  render: () => React.ReactElement;
+}
+
+class Ref<RefType, ConfigType> implements RefConfig<RefType, ConfigType> {
+  protected _ref: RefObject<RefType>;
+
+  private _id: string;
+
+  private _config: ConfigType;
+
+  constructor(config: ConfigType) {
+    this._ref = useRef<RefType>(null);
+    this._config = config;
+    this._id = createUniqueId();
+  }
+
+  get current() {
+    if (!this._ref.current) throw new Error('should match component');
+    return this._ref.current;
+  }
+
+  get config() {
+    return this._config;
+  }
+
+  get id() {
+    return this._id;
+  }
+}
+
+export class Image
+  extends Ref<Konva.Image, ImageConfig>
+  implements Utils<Konva.Image, ImageConfig>
+{
+  render() {
+    return <ImageComponent {...this.config} ref={this._ref} />;
+  }
+}
+
+export class Text
+  extends Ref<Konva.Text, TextConfig>
+  implements Utils<Konva.Text, TextConfig>
+{
+  render() {
+    return <TextComponent {...this.config} ref={this._ref} />;
+  }
+}
+
+export class Group<ChildType extends Utils<unknown, unknown>>
+  extends Ref<Konva.Group, GroupConfig>
+  implements Utils<Konva.Group, GroupConfig>
+{
+  children: ChildType[];
+
+  constructor(config: GroupConfig, children: ChildType[]) {
+    super(config);
+    this.children = children;
+  }
+
+  render() {
+    return (
+      <GroupComponent {...this.config} ref={this._ref}>
+        {this.children.map(child => child.render())}
+      </GroupComponent>
+    );
+  }
+}
+
+export class Stage<ChildType extends Utils<unknown, unknown>> extends Ref<
+  Konva.Stage,
+  ContainerConfig
+> {
+  children: ChildType[];
+
+  _ref: RefObject<Konva.Stage>;
+
+  _canvasRef: RefObject<Konva.Layer>;
+
+  constructor(config: ContainerConfig, children = [] as ChildType[]) {
+    super(config);
+    this.children = children;
+    this._ref = useRef<Konva.Stage>(null);
+    this._canvasRef = useRef<Konva.Layer>(null);
+  }
+}
