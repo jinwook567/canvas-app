64282c720889a2266ade5f5ce94a62e9d651064a
jinwook567 <jinwook567@we2d.com>
2023-04-18 02:30:37 +0900

    feat: Stage Wrapper 정의. Stage 컴포넌트가 더는 사용되지 않을 것으로 예상되어 새롭게 만듬.

---
+++ src/components/common/editor/StageWrapper/StageWrapper.tsx
@@ -0,0 +1,31 @@
+import React, { useRef, useEffect } from 'react';
+import * as Styled from './StageWrapper.styles';
+
+type Props = {
+  isSelected: boolean;
+  onSelect: () => void;
+  children: React.ReactNode;
+};
+
+function Wrapper({ isSelected, children, onSelect }: Props) {
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (isSelected && ref.current) {
+      ref.current?.focus();
+      ref.current?.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+        inline: 'nearest',
+      });
+    }
+  }, [isSelected]);
+
+  return (
+    <Styled.Div isSelected={isSelected} ref={ref} onClick={() => onSelect()}>
+      {children}
+    </Styled.Div>
+  );
+}
+
+export default Wrapper;
