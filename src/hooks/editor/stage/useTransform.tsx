57cd3ae9c0ab1237159bc5298c33bbad0c08db4a
jinwook567 <jinwook567@we2d.com>
2023-04-21 17:55:43 +0900

    refactor: 불필요한 함수 제거

--- src/hooks/editor/stage/useTransform.tsx
+++ src/hooks/editor/stage/useTransform.tsx
@@ -1,53 +1,33 @@
 import { useSetRecoilState } from 'recoil';
-import { Props as TemplateType } from '../../../components/common/editor/Preview/Preview';
-import { stageClassesState, stagesState } from '../../../recoil/editor/atoms';
-import { Stage } from '../../../types/editor';
+import { stageClassesState } from '../../../recoil/editor/atoms';
 import { getResizeScale } from '../../../utils/editor/scale';
-import { createStageSize, getSize } from '../../../utils/editor/size';
-import { giveId } from '../node/useCreate';
-import { updateStages } from '../../../utils/editor/update';
 import { Stage as StageClass } from '../../../utils/editor/shapes';
+import useSelect from './useSelect';
 
 function useTransform() {
-  const setStagesOld = useSetRecoilState(stagesState);
-
   const setStages = useSetRecoilState(stageClassesState);
+  const { selectStage } = useSelect();
 
   function applyTemplate(template: StageClass, stageToApply: StageClass) {
+    const scale = getResizeScale(
+      template.bounds.size,
+      stageToApply.bounds.size,
+      1
+    );
+
+    const duplicated = template.duplicate().setConfig({
+      ...template.config,
+      scaleX: scale,
+      scaleY: scale,
+    });
+
     setStages(stages =>
-      stages.map(stage =>
-        stage.id === stageToApply.id ? template.duplicate() : stage
-      )
+      stages.map(stage => (stage.id === stageToApply.id ? duplicated : stage))
     );
-  }
-
-  function transformSelectedStageByTemplate(
-    template: TemplateType,
-    stageToUpdate: Stage
-  ) {
-    setStagesOld(stages =>
-      updateStages(stages, {
-        ...stageToUpdate,
-        nodes: template.nodes.map(node => giveId(node)),
-        config: {
-          ...stageToUpdate.config,
-          scaleX: getTemplateScale(template, stageToUpdate),
-          scaleY: getTemplateScale(template, stageToUpdate),
-        },
-      })
-    );
-
-    function getTemplateScale(template: TemplateType, stage: Stage) {
-      return getResizeScale(
-        getSize(template.stageWidth, template.stageHeight),
-        createStageSize(stage).size,
-        1
-      );
-    }
+    selectStage(duplicated.id);
   }
 
   return {
-    transformSelectedStageByTemplate,
     applyTemplate,
   };
 }
