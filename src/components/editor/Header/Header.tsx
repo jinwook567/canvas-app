2251a0d53134efdede2bd2894fb00cc323945dc7
jinwook567 <jinwook567@we2d.com>
2023-04-21 19:54:46 +0900

    refactor: 변경된 requestExport 함수 할당

--- src/components/editor/Header/Header.tsx
+++ src/components/editor/Header/Header.tsx
@@ -1,9 +1,13 @@
+import _ from 'lodash';
 import React from 'react';
+import { useRecoilValue } from 'recoil';
 import useHistory from '../../../hooks/editor/global/useHistory';
 import useDownload from '../../../hooks/editor/stage/useDownload';
+import { stageClassesState } from '../../../recoil/editor/atoms';
 import HeadControlBar from '../../common/editor/HeadControlBar/HeadControlBar';
 
 function Header() {
+  const stages = useRecoilValue(stageClassesState);
   const { requestExport } = useDownload();
   const { historyBack, historyForward } = useHistory();
 
@@ -16,7 +20,12 @@ function Header() {
   };
 
   const handleRequestExport = () => {
-    requestExport();
+    requestExport(
+      _.chain(stages)
+        .map(stage => stage.node?.toDataURL())
+        .compact()
+        .value()
+    );
   };
 
   return (
