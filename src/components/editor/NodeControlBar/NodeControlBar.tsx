5f257cf59e9fe299d15061f464f3eb7083e95217
jinwook567 <jinwook567@we2d.com>
2023-04-21 19:52:03 +0900

    refactor: 변경된 훅 할당해줌.

--- src/components/editor/NodeControlBar/NodeControlBar.tsx
+++ src/components/editor/NodeControlBar/NodeControlBar.tsx
@@ -1,36 +1,58 @@
-import { Button, Typography } from '@mui/material';
 import React from 'react';
+import { Button, Typography } from '@mui/material';
+import _ from 'lodash';
 import { useRecoilValue } from 'recoil';
-import useDelete from '../../../hooks/editor/node/useDelete';
 import useGroup from '../../../hooks/editor/node/useGroup';
-import { selectedIdsState } from '../../../recoil/editor/atoms';
+import useRemove from '../../../hooks/editor/node/useRemove';
+import {
+  selectedIdsState,
+  stageClassesState,
+} from '../../../recoil/editor/atoms';
 import * as Styled from './NodeControlBar.styles';
 
 function NodeControlBar() {
-  const { group, canGroup, ungroup, canUnGroup } = useGroup();
-  const { deleteNodes, canDeleteNode } = useDelete();
+  const { removeNodes } = useRemove();
+  const { group, ungroup } = useGroup();
   const selectedIds = useRecoilValue(selectedIdsState);
+  const stages = useRecoilValue(stageClassesState);
+
+  const selectedShapeNodes = _.chain(stages)
+    .flatMap(stage => stage.children)
+    .filter(shape => selectedIds.includes(shape.id))
+    .map(shape => shape.node)
+    .compact()
+    .value();
+
+  const shapeStage = selectedShapeNodes[0]?.getStage();
 
   return (
     <Styled.Grid padding={1} columnGap={1} container>
       <Button
         variant="outlined"
-        disabled={!canGroup(selectedIds)}
-        onClick={() => group(selectedIds)}
+        disabled={
+          selectedShapeNodes.length < 2 ||
+          !selectedShapeNodes.every(
+            node => node.getStage() === selectedShapeNodes[0].getStage()
+          )
+        }
+        onClick={() => shapeStage && group(selectedIds, shapeStage.id())}
       >
         <Typography variant="body2">그룹화</Typography>
       </Button>
       <Button
         variant="outlined"
-        disabled={!canUnGroup(selectedIds[0])}
-        onClick={() => ungroup(selectedIds[0])}
+        disabled={
+          selectedShapeNodes.length !== 1 ||
+          selectedShapeNodes[0].getType() !== 'group'
+        }
+        onClick={() => shapeStage && ungroup(selectedIds[0], shapeStage.id())}
       >
         <Typography variant="body2">그룹 해제</Typography>
       </Button>
       <Button
         variant="outlined"
-        disabled={!canDeleteNode(selectedIds)}
-        onClick={() => deleteNodes(selectedIds)}
+        disabled={selectedIds.length === 0}
+        onClick={() => shapeStage && removeNodes(selectedIds, shapeStage.id())}
       >
         <Typography variant="body2">삭제</Typography>
       </Button>
