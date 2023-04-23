8a1b1c10de5837d641d49fb0cd58dd23dc802744
jinwook567 <jinwook567@we2d.com>
2023-04-24 05:17:40 +0900

    feat: Node 역할 확장
    unmount 될 때, 선택된 stage에 존재하지 않을 때 선택 해제 기능 추가

--- src/components/editor/Node/Node.tsx
+++ src/components/editor/Node/Node.tsx
@@ -2,18 +2,36 @@ import { ReactElement, useEffect } from 'react';
 
 type Props = {
   isSelected: boolean;
+  isInSelectedStage: boolean;
   updateTransformer: (isSelected: boolean) => void;
+  select: () => void;
+  deselect: () => void;
   render: ReactElement;
 };
 
-function Node({ isSelected, updateTransformer, render }: Props) {
+function Node({
+  isSelected,
+  isInSelectedStage,
+  updateTransformer,
+  select,
+  deselect,
+  render,
+}: Props) {
   useEffect(() => {
     updateTransformer(isSelected);
+  }, [isSelected]);
 
+  useEffect(() => {
+    select();
     return () => {
+      deselect();
       updateTransformer(false);
     };
-  }, [isSelected]);
+  }, []);
+
+  useEffect(() => {
+    if (isSelected && !isInSelectedStage) deselect();
+  }, [isSelected, isInSelectedStage]);
 
   return render;
 }
