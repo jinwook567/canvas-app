f4f65d311c1dbf03d487ca11a3d507ca5d6e6fab
jinwook567 <jinwook567@we2d.com>
2023-04-23 18:40:44 +0900

    feat: 선택 관련 이벤트 할당

--- src/components/editor/Stage/Stage.tsx
+++ src/components/editor/Stage/Stage.tsx
@@ -3,22 +3,29 @@ import Konva from 'konva';
 import { Stage as StageComponent } from 'react-konva';
 import { ContainerConfig } from 'konva/lib/Container';
 import StageWrapper from '../../common/editor/StageWrapper/StageWrapper';
+import useSelect from '../../../hooks/editor/stage/useSelect';
+import useSelectNode from '../../../hooks/editor/node/useSelect';
 
 type Props = {
+  id: string;
   children: React.ReactNode;
   setNode: (node: Konva.Stage | null) => void;
   config: ContainerConfig;
 };
 
-function Stage({ children, config, setNode }: Props) {
+function Stage({ children, config, setNode, id }: Props) {
+  const { selectStage, isSelected } = useSelect();
+  const { resetSelect } = useSelectNode();
+
   return (
-    <StageWrapper isSelected={false} onSelect={() => console.log('halo')}>
+    <StageWrapper isSelected={isSelected(id)} onSelect={() => selectStage(id)}>
       <StageComponent
         {...config}
+        id={id}
         ref={node => setNode(node)}
         style={{ background: 'white' }}
-        onTouchStart={() => ''}
-        onMouseDown={() => ''}
+        onTouchStart={e => e.target.getStage() === e.target && resetSelect()}
+        onMouseDown={e => e.target.getStage() === e.target && resetSelect()}
       >
         {children}
       </StageComponent>
