fdb29d64a0f9724fd108f7adf2dfcb17c2f5c34d
jinwook567 <jinwook567@we2d.com>
2023-04-29 00:59:24 +0900

    refactor: node 속성 제거

--- src/components/editor/NodeControlBar/NodeControlBar.tsx
+++ src/components/editor/NodeControlBar/NodeControlBar.tsx
@@ -9,6 +9,7 @@ import {
   stageClassesState,
 } from '../../../recoil/editor/atoms';
 import * as Styled from './NodeControlBar.styles';
+import { Group } from '../../../utils/editor/shapes';
 
 function NodeControlBar() {
   const { removeNodes } = useRemove();
@@ -16,43 +17,56 @@ function NodeControlBar() {
   const selectedIds = useRecoilValue(selectedIdsState);
   const stages = useRecoilValue(stageClassesState);
 
-  const selectedShapeNodes = _.chain(stages)
-    .flatMap(stage => stage.children)
-    .filter(shape => selectedIds.includes(shape.id))
-    .map(shape => shape.node)
-    .compact()
+  const selectedShapes = _.chain(stages)
+    .flatMap(stage =>
+      stage.children.map(shape => ({ shape, stageId: stage.id }))
+    )
+    .filter(({ shape }) => selectedIds.includes(shape.id))
     .value();
 
-  const shapeStage = selectedShapeNodes[0]?.getStage();
+  const isAllShapeInSameStage = selectedShapes.every(({ stageId }, index) =>
+    selectedShapes[index + 1]
+      ? stageId === selectedShapes[index + 1].stageId
+      : true
+  );
 
   return (
     <Styled.Grid padding={1} columnGap={1} container>
       <Button
         variant="outlined"
-        disabled={
-          selectedShapeNodes.length < 2 ||
-          !selectedShapeNodes.every(
-            node => node.getStage() === selectedShapeNodes[0].getStage()
+        disabled={selectedIds.length < 2 || !isAllShapeInSameStage}
+        onClick={() =>
+          group(
+            selectedShapes.map(({ shape }) => shape.id),
+            selectedShapes[0].stageId
           )
         }
-        onClick={() => shapeStage && group(selectedIds, shapeStage.id())}
       >
         <Typography variant="body2">그룹화</Typography>
       </Button>
+
       <Button
         variant="outlined"
         disabled={
-          selectedShapeNodes.length !== 1 ||
-          selectedShapeNodes[0].getType() !== 'group'
+          selectedShapes.length !== 1 ||
+          !(selectedShapes[0].shape instanceof Group)
         }
-        onClick={() => shapeStage && ungroup(selectedIds[0], shapeStage.id())}
+        onClick={() =>
+          ungroup(selectedShapes[0].shape.id, selectedShapes[0].stageId)
+        }
       >
         <Typography variant="body2">그룹 해제</Typography>
       </Button>
+
       <Button
         variant="outlined"
-        disabled={selectedIds.length === 0}
-        onClick={() => shapeStage && removeNodes(selectedIds, shapeStage.id())}
+        disabled={selectedIds.length === 0 || !isAllShapeInSameStage}
+        onClick={() =>
+          removeNodes(
+            selectedShapes.map(({ shape }) => shape.id),
+            selectedShapes[0].stageId
+          )
+        }
       >
         <Typography variant="body2">삭제</Typography>
       </Button>
