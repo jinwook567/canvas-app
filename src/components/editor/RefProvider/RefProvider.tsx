8aba59aef512f6d1eaf426fe401c572e1164b49e
jinwook567 <jinwook567@we2d.com>
2023-05-25 20:54:41 +0900

    fix: type 불일치 수정

--- src/components/editor/RefProvider/RefProvider.tsx
+++ src/components/editor/RefProvider/RefProvider.tsx
@@ -2,11 +2,11 @@ import React, { useRef, MutableRefObject } from 'react';
 import Konva from 'konva';
 
 type Props = {
-  children: (ref: MutableRefObject<Konva.Node | null>) => React.ReactNode;
+  children: (ref: MutableRefObject<Konva.Node | undefined>) => React.ReactNode;
 };
 
 function RefProvider<Node extends Konva.Node>({ children }: Props) {
-  const ref = useRef<Node>(null);
+  const ref = useRef<Node>();
   return <>{children(ref)}</>;
 }
 
