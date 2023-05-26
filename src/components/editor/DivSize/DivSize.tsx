e410f4ded172c873b3ef20bc0465c5b3ff8da565
jinwook567 <jinwook567@we2d.com>
2023-05-26 13:36:07 +0900

    refactor: DivSize 중복되는 로직 훅을 사용하여 제거

--- src/components/editor/DivSize/DivSize.tsx
+++ src/components/editor/DivSize/DivSize.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { debounceTime, Observable } from 'rxjs';
+import React from 'react';
+import useElementSize from '../../../hooks/useElementSize';
 
 type Props = {
   style?: React.CSSProperties;
@@ -7,43 +7,10 @@ type Props = {
 };
 
 function DivSize({ style, children }: Props) {
-  const divRef = useRef<HTMLDivElement>(null);
-  const [size, setSize] = useState({ width: 0, height: 0 });
-  const isInitial = useRef(true);
-
-  const resizeObserver = (element: Element) =>
-    new Observable<ResizeObserverEntry>(subscriber => {
-      const observer = new ResizeObserver(entries => {
-        entries.forEach(entry => subscriber.next(entry));
-      });
-
-      observer.observe(element);
-      return () => {
-        observer.unobserve(element);
-      };
-    });
-
-  useEffect(() => {
-    const { current } = divRef;
-    if (!current) return () => {};
-
-    const observable = resizeObserver(current);
-
-    const subscription = observable
-      .pipe(debounceTime(isInitial.current ? 0 : 300))
-      .subscribe(entry => {
-        isInitial.current = false;
-        const { width, height } = entry.contentRect;
-        setSize({ width, height });
-      });
-
-    return () => {
-      subscription.unsubscribe();
-    };
-  }, []);
+  const { size, ref } = useElementSize<HTMLDivElement>();
 
   return (
-    <div ref={divRef} style={style}>
+    <div ref={ref} style={style}>
       {children(size)}
     </div>
   );
