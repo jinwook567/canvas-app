08d787f392937e0fc84bb7647354f829fd16397f
jinwook567 <jinwook567@we2d.com>
2023-05-28 18:57:03 +0900

    fix: setRef props 추가, attachNode 함수 null 허용

--- src/components/editor/TransformableLayer/TransformableLayer.tsx
+++ src/components/editor/TransformableLayer/TransformableLayer.tsx
@@ -2,21 +2,22 @@ import React, { useRef } from 'react';
 import { Layer, Transformer } from 'react-konva';
 import Konva from 'konva';
 
-type AttchNode = <T extends Konva.Node>(node: T) => void;
-type DetachNode = (nodeId: string) => void;
+export type AttachNode = <T extends Konva.Node>(node: T | null) => void;
+export type DetachNode = (nodeId: string) => void;
 
 type Props = {
   children: ({
     attachNode,
     detachNode,
   }: {
-    attachNode: AttchNode;
+    attachNode: AttachNode;
     detachNode: DetachNode;
   }) => React.ReactNode;
   onTransform: (transformedNodes: Konva.Node[]) => void;
+  setRef: (layer: Konva.Layer | null) => void;
 };
 
-function TransformableLayer({ children, onTransform }: Props) {
+function TransformableLayer({ children, onTransform, setRef }: Props) {
   const ref = useRef<Konva.Transformer>(null);
 
   const handleTransform = () => {
@@ -27,8 +28,9 @@ function TransformableLayer({ children, onTransform }: Props) {
     if (ref.current) ref.current.getLayer()?.batchDraw();
   };
 
-  const attachNode: AttchNode = node => {
+  const attachNode: AttachNode = node => {
     if (!ref.current) return;
+    if (!node) return;
 
     ref.current.nodes([...ref.current.nodes(), node]);
     batchDraw();
@@ -43,7 +45,7 @@ function TransformableLayer({ children, onTransform }: Props) {
 
   return (
     <>
-      <Layer>{children({ attachNode, detachNode })}</Layer>
+      <Layer ref={setRef}>{children({ attachNode, detachNode })}</Layer>
       <Layer>
         <Transformer
           ref={ref}
