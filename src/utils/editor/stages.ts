e48b8834aa4963b25200f09098eff26fec36cb63
jinwook567 <jinwook567@we2d.com>
2023-06-02 00:04:19 +0900

    refactor: stages를 다루는 함수들 객체 형태로 반환 및 add 함수 여러개 넣을 수 있도록 수정

--- src/utils/editor/stages.ts
+++ src/utils/editor/stages.ts
@@ -3,27 +3,44 @@ import { Stage } from './node';
 
 export type Stages = Stage[];
 
-export const equals = curry(
-  (stage1: Stage, stage2: Stage) => stage1.id === stage2.id
-);
+const equals = curry((stage1: Stage, stage2: Stage) => stage1.id === stage2.id);
 
-export const notEquals = curry(
+const notEquals = curry(
   (stage1: Stage, stage2: Stage) => stage1.id !== stage2.id
 );
 
-export const map = curry(<T>(f: (stage: Stage) => T, stages: Stages) =>
-  stages.map(f)
-);
+const map = curry(<T>(f: (stage: Stage) => T, stages: Stages) => stages.map(f));
 
-export const filter = curry((f: (stage: Stage) => boolean, stages: Stages) =>
+const filter = curry((f: (stage: Stage) => boolean, stages: Stages) =>
   stages.filter(f)
 );
 
-export const reduce = curry(
-  <T>(f: (acc: T, stage: Stage) => T, initialValue: T, stages: Stages) =>
+const reduce = curry(
+  <T>(f: (acc: T, stage: Stage) => T, initialValue: T, stages: Stages): T =>
     stages.reduce((acc, stage) => f(acc, stage), initialValue)
 );
 
-export const replace = curry((stageToReplace: Stage, stages: Stages) =>
-  map(stage => (equals(stage, stageToReplace) ? stageToReplace : stage), stages)
+const replace = curry(
+  (stageToReplace: Stage, stages: Stages): Stages =>
+    map(
+      stage => (equals(stage, stageToReplace) ? stageToReplace : stage),
+      stages
+    )
 );
+
+const add =
+  (...stagesToAdd: Stages) =>
+  (stages: Stages) =>
+    [...stages, ...stagesToAdd];
+
+const S = {
+  equals,
+  notEquals,
+  map,
+  filter,
+  reduce,
+  replace,
+  add,
+};
+
+export default S;
