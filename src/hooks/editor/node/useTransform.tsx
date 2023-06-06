a24efbf806a931ed453f5797b751d112ede02e70
jinwook567 <jinwook567@we2d.com>
2023-06-07 02:27:02 +0900

    refactor: Konva.Node 배열도 받을 수 있도록하여 다형성을 높였음.

--- src/hooks/editor/node/useTransform.tsx
+++ src/hooks/editor/node/useTransform.tsx
@@ -1,17 +1,40 @@
+import Konva from 'konva';
 import { useSetRecoilState } from 'recoil';
 import { stagesState } from '../../../recoil/editor/atoms';
-import { Child } from '../../../utils/editor/node';
+import { Child, isNode } from '../../../utils/editor/node';
 import S from '../../../utils/editor/stages';
 
 function useTransform() {
   const setStages = useSetRecoilState(stagesState);
 
-  const transformNodes = (nodes: Child[]) => {
+  const handleNodes = (nodes: Child[]) => {
     const update = (child: Child) => {
       const nodeToTransform = nodes.find(node => node.equals(child));
       return nodeToTransform || child;
     };
     setStages(S.map(stage => stage.mapChild(update)));
+  };
+
+  const handleKonvaNodes = (nodes: Konva.Node[]) => {
+    const update = (child: Child) => {
+      const node = nodes.find(node => node.id() === child.id);
+      if (node) return child.map(config => ({ ...config, ...node.attrs }));
+      return child;
+    };
+    setStages(S.map(stage => stage.mapChild(update)));
+  };
+
+  function notKonvaNode(nodes: Child[] | Konva.Node[]): nodes is Child[] {
+    if (nodes.length === 0) return true;
+    return isNode(nodes[0]);
+  }
+
+  const transformNodes = (nodes: Child[] | Konva.Node[]) => {
+    if (notKonvaNode(nodes)) {
+      handleNodes(nodes);
+    } else {
+      handleKonvaNodes(nodes);
+    }
   };
 
   return {
