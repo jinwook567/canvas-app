5e59a9c1e21ebc9a71c7ce20ea1f8ed71b796ca9
jinwook567 <jinwook567@we2d.com>
2023-06-07 02:42:23 +0900

    feat: StageList
    List 컴포넌트 명칭을 더 명확한 StageList로 변경 및TransformableLayer, NodeList 컴포넌트를 활용하여 개선

---
+++ src/pages/editor/StageList/StageList.tsx
@@ -0,0 +1,83 @@
+import React from 'react';
+import Konva from 'konva';
+import NodeList from '../NodeList/NodeList';
+import useElementSize from '../../../hooks/useElementSize';
+import Stage from '../Stage/Stage';
+import * as Styled from './StageList.styles';
+import StageControlBar from '../StageControlBar/StageControlBar';
+import NodeControlBar from '../NodeControlBar/NodeControlBar';
+import TransformableLayer from '../../../components/editor/TransformableLayer/TransformableLayer';
+import useTransform from '../../../hooks/editor/node/useTransform';
+import { Stage as StageType } from '../../../utils/editor/node';
+import StageSelector from '../../../components/editor/StageSelector/StageSelector';
+import useSelect from '../../../hooks/editor/stage/useSelect';
+
+type Props = {
+  setRef: (id: string, node: Konva.Layer | null) => void;
+  items: StageType[];
+};
+
+function StageList({ setRef, items }: Props) {
+  const { size, ref } = useElementSize<HTMLDivElement>();
+  const { transformNodes } = useTransform();
+  const { selectStage } = useSelect();
+
+  const handleStageDeselect =
+    (prevStage: StageType, nextStage: StageType) => () => {
+      if (nextStage) {
+        selectStage(nextStage);
+      } else if (prevStage) {
+        selectStage(prevStage);
+      } else {
+        selectStage(null);
+      }
+    };
+
+  return (
+    <Styled.Grid ref={ref} rowGap={3}>
+      <NodeControlBar />
+      {items.map((stage, index) => {
+        const prevStage = items[index - 1];
+        const nextStage = items[index + 1];
+
+        return (
+          <div key={stage.id}>
+            <StageControlBar
+              stage={stage}
+              prevStage={prevStage}
+              nextStage={nextStage}
+            />
+
+            <StageSelector
+              onSelect={() => selectStage(stage)}
+              onDeselect={handleStageDeselect(prevStage, nextStage)}
+            >
+              <Stage
+                stage={stage}
+                key={stage.id}
+                parentSize={size}
+                parentRatio={0.65}
+              >
+                <TransformableLayer
+                  setRef={layer => setRef(stage.id, layer)}
+                  onTransform={transformNodes}
+                >
+                  {({ attachNode, detachNode }) => (
+                    <NodeList
+                      updateTransformer={(node, id, isSelected) =>
+                        isSelected ? attachNode(node) : detachNode(id)
+                      }
+                      items={stage.children}
+                    />
+                  )}
+                </TransformableLayer>
+              </Stage>
+            </StageSelector>
+          </div>
+        );
+      })}
+    </Styled.Grid>
+  );
+}
+
+export default StageList;
