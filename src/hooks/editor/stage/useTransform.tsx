ff322b4235e8869d5ccf44c22d17248a5f6b7957
jinwook567 <jinwook567@we2d.com>
2023-06-07 02:27:43 +0900

    refactor: 선택 관련 로직은 클라이언트에게 위임

--- src/hooks/editor/stage/useTransform.tsx
+++ src/hooks/editor/stage/useTransform.tsx
@@ -2,17 +2,12 @@ import { useSetRecoilState } from 'recoil';
 import { stagesState } from '../../../recoil/editor/atoms';
 import { Stage } from '../../../utils/editor/node';
 import S from '../../../utils/editor/stages';
-import useSelect from './useSelect';
 
 function useTransform() {
   const setStages = useSetRecoilState(stagesState);
-  const { selectStage } = useSelect();
 
   function applyTemplate(template: Stage, stageToApply: Stage) {
-    const duplicated = template.duplicate();
-
-    setStages(S.replace(stageToApply, duplicated));
-    selectStage(duplicated);
+    setStages(S.replace(stageToApply, template.duplicate()));
   }
 
   return {
