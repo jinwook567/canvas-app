76457c3343d31242af9ac7ac896f4e19262114a4
jinwook567 <jinwook567@we2d.com>
2023-06-07 02:39:41 +0900

    refactor: Stage
    선택과 관련된 역할 삭제

--- src/pages/editor/Stage/Stage.tsx
+++ src/pages/editor/Stage/Stage.tsx
@@ -1,46 +1,33 @@
 import React from 'react';
-import Konva from 'konva';
-import { ContainerConfig } from 'konva/lib/Container';
 import StageWrapper from '../../../components/editor/StageWrapper/StageWrapper';
+import { Stage as StageType } from '../../../utils/editor/node';
 import useSelect from '../../../hooks/editor/stage/useSelect';
 import useSelectNode from '../../../hooks/editor/node/useSelect';
 import ResponsiveStage from '../../../components/editor/ResponsiveStage/ResponsiveStage';
 import { Size } from '../../../utils/editor/size';
 
 type Props = {
-  id: string;
+  stage: StageType;
   children: React.ReactNode;
-  setNode?: (node: Konva.Stage | null) => void;
-  config: ContainerConfig;
-  size: Size;
   parentSize: Size;
   parentRatio: number;
 };
 
-function Stage({
-  children,
-  config,
-  setNode,
-  id,
-  size,
-  parentSize,
-  parentRatio,
-}: Props) {
-  const { selectStage, isSelected } = useSelect();
+function Stage({ stage, children, parentSize, parentRatio }: Props) {
+  const { isSelected } = useSelect();
   const { resetSelect } = useSelectNode();
 
   return (
-    <StageWrapper isSelected={isSelected(id)} onSelect={() => selectStage(id)}>
+    <StageWrapper isSelected={isSelected(stage)}>
       <ResponsiveStage
-        size={size}
+        id={stage.id}
+        size={stage.bounds.size}
         parentSize={parentSize}
         parentRatio={parentRatio}
-        id={id}
         style={{ background: 'white' }}
         onTouchStart={e => e.target.getStage() === e.target && resetSelect()}
         onMouseDown={e => e.target.getStage() === e.target && resetSelect()}
-        setRef={setNode}
-        {...config}
+        {...stage.config}
       >
         {children}
       </ResponsiveStage>
