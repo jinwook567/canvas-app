1583906e01b0292735d52b5d083d9c2767a1cd9a
jinwook567 <jinwook567@we2d.com>
2023-06-14 22:01:30 +0900

    feat: DivSize inherit property 추가

--- src/components/common/DivSize/index.tsx
+++ src/components/common/DivSize/index.tsx
@@ -4,13 +4,17 @@ import useElementSize from 'hooks/useElementSize';
 type Props = {
   style?: React.CSSProperties;
   children: (size: { width: number; height: number }) => React.ReactNode;
+  inherit?: boolean;
 };
 
-function DivSize({ style, children }: Props) {
+function DivSize({ style, children, inherit }: Props) {
   const { size, ref } = useElementSize<HTMLDivElement>();
 
   return (
-    <div ref={ref} style={style}>
+    <div
+      ref={ref}
+      style={inherit ? { width: '100%', height: '100%', ...style } : style}
+    >
       {children(size)}
     </div>
   );
