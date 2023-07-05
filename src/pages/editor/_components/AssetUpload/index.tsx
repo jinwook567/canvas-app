cfa176c0738d8cf788cf6b10f8e5860fc1c52aa3
jinwook567 <jinwook567@we2d.com>
2023-07-06 04:26:43 +0900

    feat: AssetUpload

---
+++ src/pages/editor/_components/AssetUpload/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Grid } from '@mui/material';
+import ImageUpload from 'components/editor/ImageUpload';
+import AssetList from 'pages/editor/_components/AssetList';
+import { Node } from 'utils/editor/node';
+
+type Props = {
+  onUpload: (image: string) => void;
+  uploadedAssets: string[];
+  onClick: (node: Node) => void;
+};
+
+function AssetUpload({ onClick, onUpload, uploadedAssets }: Props) {
+  return (
+    <Grid container flexDirection="column" alignItems="center" rowGap={3}>
+      <ImageUpload onComplete={onUpload} />
+      <AssetList
+        assets={[{ type: 'mansory', nodes: uploadedAssets }]}
+        addAsset={onClick}
+      />
+    </Grid>
+  );
+}
+
+export default AssetUpload;
