350c2f2e97548bba5eab83467a2b74d3d236b4b5
jinwook567 <jinwook567@we2d.com>
2023-07-07 19:36:02 +0900

    feat: LayoutHandler

---
+++ src/components/editor/LayoutHandler/index.tsx
@@ -0,0 +1,73 @@
+import React, { useState } from 'react';
+import { DragOverlay, UniqueIdentifier } from '@dnd-kit/core';
+import { Paper } from '@mui/material';
+import DivSize from 'components/common/DivSize';
+import SortableDnd from 'components/common/SortableDnd';
+import PreviewShape from 'components/editor/PreviewShape';
+import { Shape } from 'utils/editor/node';
+
+type Props = {
+  onChange: (itmes: Props['items']) => void;
+  items: Shape[];
+};
+
+function LayoutHandler({ onChange, items }: Props) {
+  const [activeId, setActiveId] = useState<UniqueIdentifier | null>(null);
+
+  const handleSelect = (id: UniqueIdentifier) => {
+    setActiveId(id);
+  };
+
+  const handleItems = (f: (items: Shape[]) => Shape[]) => {
+    onChange(f(items));
+  };
+
+  const selectedItem = items.find(item => item.id === activeId);
+
+  return (
+    <SortableDnd onDragEnd={handleItems} onDragStart={handleSelect}>
+      <SortableDnd.Context items={items.map(item => item.id)}>
+        {items.map(item => (
+          <SortableDnd.Item id={item.id} key={item.id}>
+            <Item shape={item} />
+          </SortableDnd.Item>
+        ))}
+      </SortableDnd.Context>
+      {selectedItem && (
+        <DragOverlay>
+          <Item shape={selectedItem} />
+        </DragOverlay>
+      )}
+    </SortableDnd>
+  );
+}
+
+type ItemProps = {
+  shape: Shape;
+};
+
+function Item({ shape }: ItemProps) {
+  return (
+    <Paper
+      elevation={1}
+      sx={{
+        padding: 0.5,
+        height: 50,
+        mb: 1,
+      }}
+    >
+      <DivSize
+        inherit
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        {size => <PreviewShape shape={shape} parentSize={size} />}
+      </DivSize>
+    </Paper>
+  );
+}
+
+export default LayoutHandler;
