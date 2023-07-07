927f01e076d76514c6bcf8448b49ce216997ef3a
jinwook567 <jinwook567@we2d.com>
2023-07-08 02:40:13 +0900

    refactor: ShapeControlBar
    공통 components에 위치할 필요가 없다고 생각하여 pages 내부의 components로 인라인 시켰음. 그리고 위치 기능을 추가시켰음.

--- src/pages/editor/_components/ShapeControlBar/index.tsx
+++ src/pages/editor/_components/ShapeControlBar/index.tsx
@@ -1,35 +1,83 @@
 import React from 'react';
-import { useRecoilValue } from 'recoil';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
 import useGroup from 'hooks/editor/node/useGroup';
 import useRemove from 'hooks/editor/node/useRemove';
-import { selectedNodesState } from 'recoils/editor/atoms';
-import * as Styled from 'pages/editor/_components/ShapeControlBar/styles';
-import ControlBar from 'components/editor/ShapeControlBar';
+import {
+  selectedNodesState,
+  selectedTabState,
+  tabValue,
+} from 'recoils/editor/atoms';
 import { Group } from 'utils/editor/node';
+import { Button, Grid, Typography } from '@mui/material';
 
 function ShapeControlBar() {
   const { removeNodes } = useRemove();
   const { group, ungroup } = useGroup();
   const selectedNodes = useRecoilValue(selectedNodesState);
+  const setSelectedTab = useSetRecoilState(selectedTabState);
+
+  const canGroup = selectedNodes.length >= 2;
+  const canRemove = selectedNodes.length >= 1;
+  const canUnGroup =
+    selectedNodes.length === 1 && selectedNodes[0].type === 'group';
+  const canLocate = selectedNodes.length >= 1;
+
+  const buttons = [
+    {
+      name: '그룹화',
+      onClick: canGroup ? () => group(selectedNodes) : null,
+    },
+    {
+      name: '그룹해제',
+      onClick: canUnGroup ? () => ungroup(selectedNodes[0] as Group) : null,
+    },
+    {
+      name: '삭제',
+      onClick: canRemove ? () => removeNodes(selectedNodes) : null,
+    },
+    {
+      name: '위치',
+      onClick: canLocate
+        ? () => setSelectedTab(tabValue.handlerPosition)
+        : null,
+    },
+  ];
 
   return (
-    <Styled.Grid padding={1} columnGap={1} container>
-      <ControlBar
-        onGroup={
-          (selectedNodes.length >= 2 || undefined) &&
-          (() => group(selectedNodes))
-        }
-        onRemove={
-          (selectedNodes.length >= 1 || undefined) &&
-          (() => removeNodes(selectedNodes))
-        }
-        onUnGroup={
-          ((selectedNodes.length === 1 && selectedNodes[0].type === 'group') ||
-            undefined) &&
-          (() => ungroup(selectedNodes[0] as Group))
-        }
-      />
-    </Styled.Grid>
+    <Grid
+      padding={1}
+      columnGap={1}
+      container
+      sx={{
+        background: 'white',
+        width: 1,
+        position: 'sticky',
+        top: 0,
+        zIndex: 999,
+      }}
+    >
+      {buttons.map(({ name, onClick }, index) => (
+        <ControlButton name={name} onClick={onClick} key={index} />
+      ))}
+    </Grid>
+  );
+}
+
+function ControlButton({
+  name,
+  onClick,
+}: {
+  name: string;
+  onClick: (() => void) | null;
+}) {
+  return (
+    <Button
+      variant="outlined"
+      disabled={!onClick}
+      onClick={() => onClick && onClick()}
+    >
+      <Typography variant="body2">{name}</Typography>
+    </Button>
   );
 }
 
