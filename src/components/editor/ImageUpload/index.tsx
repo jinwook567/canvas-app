9fe8e43a46b76d5a7473a82c6e86acad97a1a121
jinwook567 <jinwook567@we2d.com>
2023-07-22 01:13:42 +0900

    refactor: ImageUpload 모듈 최대한 작게 유지

--- src/components/editor/ImageUpload/index.tsx
+++ src/components/editor/ImageUpload/index.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import { Button } from '@mui/material';
-import FileUploadIcon from '@mui/icons-material/FileUpload';
 
 type Props = {
   onComplete: (base64Image: string) => void;
@@ -17,21 +15,13 @@ function ImageUpload({ onComplete }: Props) {
   };
 
   return (
-    <Button
-      variant="outlined"
-      sx={{ borderRadius: '4px' }}
-      startIcon={<FileUploadIcon />}
-      component="label"
-    >
-      이미지 업로드
-      <input
-        hidden
-        accept="image/*"
-        multiple
-        type="file"
-        onChange={handleChange}
-      />
-    </Button>
+    <input
+      hidden
+      accept="image/*"
+      multiple
+      type="file"
+      onChange={handleChange}
+    />
   );
 }
 
--- src/pages/editor/components/AssetUpload/index.tsx
+++ src/pages/editor/components/AssetUpload/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Grid } from '@mui/material';
+import { Button, Grid } from '@mui/material';
 import ImageUpload from 'components/editor/ImageUpload';
 import AssetList from 'pages/editor/components/AssetList';
 import { Node } from 'utils/editor/node';
+import FileUpload from '@mui/icons-material/FileUpload';
 
 type Props = {
   onUpload: (image: string) => void;
@@ -13,7 +14,16 @@ type Props = {
 function AssetUpload({ onClick, onUpload, uploadedAssets }: Props) {
   return (
     <Grid container flexDirection="column" alignItems="center" rowGap={3}>
-      <ImageUpload onComplete={onUpload} />
+      <Button
+        variant="outlined"
+        sx={{ borderRadius: '4px' }}
+        startIcon={<FileUpload />}
+        component="label"
+      >
+        이미지 업로드
+        <ImageUpload onComplete={onUpload} />
+      </Button>
+
       <AssetList
         assets={[{ type: 'mansory', nodes: uploadedAssets }]}
         addAsset={onClick}
