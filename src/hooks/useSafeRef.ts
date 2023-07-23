8ab0184e4b5089193c23b198581dc359c84a6f87
jinwook567 <jinwook567@we2d.com>
2023-07-23 20:11:30 +0900

    feat: useSafeRef

---
+++ src/hooks/useSafeRef.ts
@@ -0,0 +1,28 @@
+import { useRef } from 'react';
+import Maybe from 'utils/maybe';
+
+type SetRef<T> = (node: T) => void;
+type MapRef<T> = <P>(
+  f: (arg: NonNullable<T>) => P
+) => ReturnType<typeof Maybe.fromNullable<P | null>>;
+
+function useSafeRef<T>() {
+  const ref = useRef<T>(null);
+
+  const set = <T>(ref: { current: T }, node: T) => {
+    ref.current = node;
+  };
+
+  const setRef: SetRef<T> = node => {
+    set(ref, node);
+  };
+
+  const mapRef: MapRef<T> = f =>
+    Maybe.fromNullable(ref.current ? f(ref.current) : null);
+
+  const safeRef = () => Maybe.fromNullable(ref.current);
+
+  return { setRef, mapRef, safeRef };
+}
+
+export default useSafeRef;
