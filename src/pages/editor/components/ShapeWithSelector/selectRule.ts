d1c430b99eee863b295007b76fd0fb19cea9e1c9
jinwook567 <jinwook567@we2d.com>
2023-07-23 20:12:12 +0900

    feat: ShapeWithSelector

---
+++ src/pages/editor/components/ShapeWithSelector/selectRule.ts
@@ -0,0 +1,22 @@
+import Konva from 'konva';
+import useSelect from 'hooks/editor/stage/useSelect';
+import useSelectShape from 'hooks/editor/node/useSelect';
+import Maybe from 'utils/maybe';
+import { useEffect } from 'react';
+import { Shape } from 'utils/editor/node';
+
+function useApplySelectRules(konvaNode: Konva.Node | null, shape: Shape) {
+  const { selectedStage } = useSelect();
+  const { deselect } = useSelectShape();
+
+  const isInSelectedStage = Maybe.fromNullable(konvaNode)
+    .map(node => node.getStage())
+    .map(stage => stage.id())
+    .map(id => id === selectedStage?.id);
+
+  useEffect(() => {
+    if (!isInSelectedStage) deselect(shape);
+  }, [isInSelectedStage, shape]);
+}
+
+export default useApplySelectRules;
