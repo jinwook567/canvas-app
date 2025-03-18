f78790cadb857af7d636b9eaf2426bd9269b6d99
jinwook567 <70192587+jinwook567@users.noreply.github.com>
2025-03-18 23:40:39 +0900

    feat: Container type 재귀적인 Group 허용

--- src/features/container/model/register.ts
+++ src/features/container/model/register.ts
@@ -3,15 +3,17 @@ import { Layer, LayerConfig, shapes as layerShapes } from 'entities/layer';
 import { withPartial } from 'shared/ui';
 import { Type, Config } from 'features/shape';
 
-export const group = (
-  config: GroupConfig<Config<Type> | GroupConfig<Config<Type>>>
-) => ({
+interface Container<T> extends GroupConfig<T | Container<T>> {}
+
+type Child<T> = T | Container<T>;
+
+export const group = (config: GroupConfig<Child<Config<Type>>>) => ({
   config,
   Component: withPartial(Group, config),
   shapes: groupShapes(config),
 });
 
-export const layer = (config: LayerConfig<Config<Type>>) => ({
+export const layer = (config: LayerConfig<Child<Config<Type>>>) => ({
   config,
   Component: withPartial(Layer, config),
   shapes: layerShapes(config),
