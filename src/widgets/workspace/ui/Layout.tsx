90706c72d4b658b86345fe622f051e328afd7f9e
이진욱 <dlwlsdnr567@naver.com>
2025-04-01 03:47:30 +0900

    feat: workspace layout

---
+++ src/widgets/workspace/ui/Layout.tsx
@@ -0,0 +1,59 @@
+import { Grid, styled } from '@mui/material';
+
+export const Header = styled(Grid)`
+  padding: ${({ theme }) => theme.spacing(1)};
+  background: linear-gradient(to right, #00c4cc, #7300e6);
+`;
+
+export const Main = styled(Grid)`
+  flex: 1;
+  display: flex;
+`;
+
+export const Footer = styled(Grid)`
+  padding: ${({ theme }) => theme.spacing(1)};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: gray;
+`;
+
+export const Container = styled(Grid)`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const StickyContainer = styled(Grid)`
+  background: white;
+  width: 100%;
+  position: sticky;
+  top: 0;
+  z-index: 999;
+  padding: ${({ theme }) => theme.spacing(1)};
+`;
+
+export function CanvasSection({
+  children,
+  onClick,
+}: {
+  children: React.ReactNode;
+  onClick?: () => void;
+}) {
+  return (
+    <Grid sx={{ width: 1, height: 1, position: 'relative' }} onClick={onClick}>
+      <Grid
+        sx={{
+          width: 1,
+          height: 1,
+          overflowY: 'auto',
+          overflowX: 'hidden',
+          position: 'absolute',
+          background: 'lightgray',
+        }}
+      >
+        {children}
+      </Grid>
+    </Grid>
+  );
+}
