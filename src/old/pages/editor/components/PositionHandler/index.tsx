1e8c8b29119f6708acff635966914b7c309bbe1d
이진욱 <dlwlsdnr567@naver.com>
2025-04-01 05:57:56 +0900

    etc: delete legacy code

--- src/old/pages/editor/components/PositionHandler/index.tsx
+++
@@ -1,8 +0,0 @@
-import LayoutHandler from 'old/pages/editor/components/_handler/LayoutHandler';
-import React from 'react';
-
-function PositionHandler() {
-  return <LayoutHandler />;
-}
-
-export default PositionHandler;
--- src/old/pages/editor/components/StageList/styles.ts
+++
@@ -1,9 +0,0 @@
-import { Grid, styled } from '@mui/material';
-
-export const Container = styled(Grid)`
-  width: 100%;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
